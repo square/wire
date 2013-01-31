a2541774ab8ceb7a401e78b56eafe39a438f696e
Jake Wharton <jw@squareup.com>
2013-01-31 15:43:30 -0800

    Simple docs and simpler licenses.

--- protoparser/src/main/java/com/squareup/proto/EnumType.java
+++ protoparser/src/main/java/com/squareup/proto/EnumType.java
@@ -1,27 +1,11 @@
-/*
- * Copyright (C) 2012 Square, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2013 Square, Inc.
 package com.squareup.proto;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-/**
- * An enumerated type declaration.
- */
+/** An enumerated type declaration. */
 public final class EnumType implements Type {
   private final String name;
   private final String documentation;
@@ -55,8 +39,8 @@ public final class EnumType implements Type {
   @Override public boolean equals(Object other) {
     if (other instanceof EnumType) {
       EnumType that = (EnumType) other;
-      return name.equals(that.name)
-          && documentation.equals(that.documentation)
+      return name.equals(that.name) //
+          && documentation.equals(that.documentation) //
           && values.equals(that.values);
     }
     return false;
@@ -91,8 +75,8 @@ public final class EnumType implements Type {
     @Override public boolean equals(Object other) {
       if (other instanceof Value) {
         Value that = (Value) other;
-        return name.equals(that.name)
-            && tag == that.tag
+        return name.equals(that.name) //
+            && tag == that.tag //
             && documentation.equals(that.documentation);
       }
       return false;
