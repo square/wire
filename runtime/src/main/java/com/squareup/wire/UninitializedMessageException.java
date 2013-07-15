e4d315e282d39e955e4e56ae681ecd0302e0b2f4
Dan Rice <rice@squareup.com>
2013-07-15 15:04:18 -0400

    Javadoc improvements

--- runtime/src/main/java/com/squareup/wire/UninitializedMessageException.java
+++ runtime/src/main/java/com/squareup/wire/UninitializedMessageException.java
@@ -1,6 +1,10 @@
 // Copyright 2013 Square, Inc.
 package com.squareup.wire;
 
+/**
+ * An exception thrown from a builder's {@link com.squareup.wire.Message.Builder#build()} method
+ * when not all required fields have been set.
+ */
 public class UninitializedMessageException extends RuntimeException {
 
   public UninitializedMessageException() {
