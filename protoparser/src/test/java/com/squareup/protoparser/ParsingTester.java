94bdce9d8451c56544e62667d8bc006b44e47b07
Jake Wharton <jw@squareup.com>
2013-12-29 17:06:03 -0800

    Add simple directory-traversing parser for quickly testing the parser.

---
+++ protoparser/src/test/java/com/squareup/protoparser/ParsingTester.java
@@ -0,0 +1,38 @@
+package com.squareup.protoparser;
+
+import java.io.File;
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Deque;
+
+/** Recursively traverse a directory and attempt to parse all of its proto files. */
+public class ParsingTester {
+  /** Directory under which to search for protos. Change as needed. */
+  private static final File ROOT = new File("/path/to/protos");
+
+  public static void main(String... args) {
+    int total = 0;
+    int failed = 0;
+
+    Deque<File> fileQueue = new ArrayDeque<File>();
+    fileQueue.add(ROOT);
+    while (!fileQueue.isEmpty()) {
+      File file = fileQueue.removeFirst();
+      if (file.isDirectory()) {
+        Collections.addAll(fileQueue, file.listFiles());
+      } else if (file.getName().endsWith(".proto")) {
+        System.out.println("Parsing " + file.getPath());
+        total += 1;
+
+        try {
+          ProtoSchemaParser.parse(file);
+        } catch (Exception e) {
+          e.printStackTrace();
+          failed += 1;
+        }
+      }
+    }
+
+    System.out.println("\nTotal: " + total + "  Failed: " + failed);
+  }
+}
