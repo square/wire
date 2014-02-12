b1103f044828d03502c107c57f84c0313ac5413f
Jake Wharton <jw@squareup.com>
2014-02-12 13:13:53 -0800

    Pass FileSystem to plugins for generation.

--- wire-compiler/src/main/java/com/squareup/wire/cli/Main.java
+++ wire-compiler/src/main/java/com/squareup/wire/cli/Main.java
@@ -1,20 +1,15 @@
 package com.squareup.wire.cli;
 
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableSet;
 import com.squareup.protoparser.ProtoFile;
 import com.squareup.wire.parser.WireParser;
 import com.squareup.wire.plugin.WireJavaPlugin;
 import com.squareup.wire.plugin.WirePlugin;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 /**
  * Runs the parser and plugins.
@@ -33,14 +28,11 @@ import java.util.regex.Pattern;
  * such that only desired message types are generated.<p>If no types are specified,
  * every type in the specified proto files will be used.</li>
  * <li>{@code --plugin} &ndash; Fully-qualified class name of a plugin for code generation.<p>Each
- * plugin has a {@link WirePlugin#getArgumentPrefix() prefix} with which you can pass additional
- * arguments. See the individual plugin documentation for a list of its supported arguments.<p>If
- * no plugin is specified, the {@link WireJavaPlugin built-in Wire plugin for Java} will be
- * used.</li>
+ * plugin has arguments which you can specify with {@code -Dname=value}. See the individual plugin
+ * documentation for a list of its supported arguments.<p>If no plugin is specified, the
+ * {@link WireJavaPlugin built-in Wire plugin for Java} will be used.</li>
  * <li>{@code file} &ndash; An optional list of proto files to parse.<p>If no proto files are
  * specified, every file in the specified directories will be used.</li>
- * <li>All other "{@code --}"-prefixed arguments will be handed off to the corresponding
- * plugin.</li>
  * </ul>
  * Each argument can be specified multiple times. For example, to specify multiple roots use:
  * {@code --root=com.example.Foo --root=com.example.Bar foobar.proto}
@@ -51,12 +43,12 @@ public final class Main {
   private static final String ARG_PATH = "--path=";
   private static final String ARG_ROOT = "--root=";
   private static final String ARG_PLUGIN = "--plugin=";
-  private static final Pattern PLUGIN_PREFIX = Pattern.compile("^[a-z]+$");
+  private static final String ARG_PROPERTY = "-D";
 
   public static void main(String... args) throws IOException {
-    WireParser parser = WireParser.create();
-    Set<String> pluginClassNames = new LinkedHashSet<String>();
-    List<String> unusedArgs = new ArrayList<String>();
+    FileSystem fs = FileSystems.getDefault();
+    WireParser parser = WireParser.createWithFileSystem(fs);
+    Set<String> pluginClassNames = new LinkedHashSet<>();
 
     for (String arg : args) {
       if (arg.startsWith(ARG_PLUGIN)) {
@@ -64,14 +56,15 @@ public final class Main {
         pluginClassNames.add(pluginArg);
       } else if (arg.startsWith(ARG_PATH)) {
         String pathArg = arg.substring(ARG_PATH.length());
-        parser.addDirectory(Paths.get(pathArg));
+        parser.addDirectory(fs.getPath(pathArg));
       } else if (arg.startsWith(ARG_ROOT)) {
         String rootsArg = arg.substring(ARG_ROOT.length());
         parser.addTypeRoot(rootsArg);
-      } else if (!arg.startsWith("--")) {
-        parser.addProto(Paths.get(arg));
+      } else if (arg.startsWith(ARG_PROPERTY)) {
+        String[] parts = arg.substring(ARG_PROPERTY.length()).split("=", -1);
+        System.setProperty(parts[0], parts[1]);
       } else {
-        unusedArgs.add(arg);
+        parser.addProto(fs.getPath(arg));
       }
     }
 
@@ -85,37 +78,22 @@ public final class Main {
     Set<ProtoFile> data = parser.parse();
 
     for (WirePlugin plugin : plugins) {
-      plugin.generate(argsForPlugin(unusedArgs, plugin), data);
+      plugin.generate(fs, data);
     }
   }
 
   /** TODO tests. */
   private static Set<WirePlugin> loadPlugins(Set<String> pluginClassNames) {
-    Set<WirePlugin> plugins = new LinkedHashSet<WirePlugin>();
-    Set<String> pluginPrefixes = new LinkedHashSet<String>();
-    Set<String> pluginErrors = new LinkedHashSet<String>();
+    Set<WirePlugin> plugins = new LinkedHashSet<>();
+    Set<String> pluginErrors = new LinkedHashSet<>();
     for (String pluginClassName : pluginClassNames) {
       try {
         Class<?> pluginClass = Class.forName(pluginClassName);
         WirePlugin plugin = (WirePlugin) pluginClass.newInstance();
-
-        String pluginPrefix = plugin.getArgumentPrefix();
-        if (!PLUGIN_PREFIX.matcher(pluginPrefix).matches()) {
-          pluginErrors.add("Invalid argument prefix \"" + pluginPrefix + "\": " + pluginClassName);
-          continue;
-        }
-        if (pluginPrefixes.contains(pluginPrefix)) {
-          pluginErrors.add("Argument prefix collision: " + pluginPrefix);
-          continue;
-        }
-        pluginPrefixes.add(pluginPrefix);
-
         plugins.add(plugin);
       } catch (ClassNotFoundException e) {
         pluginErrors.add("Could not load " + pluginClassName + ": " + e.getMessage());
-      } catch (InstantiationException e) {
-        pluginErrors.add("Could not instantiate " + pluginClassName + ": " + e.getMessage());
-      } catch (IllegalAccessException e) {
+      } catch (InstantiationException | IllegalAccessException e) {
         pluginErrors.add("Could not instantiate " + pluginClassName + ": " + e.getMessage());
       }
     }
@@ -127,20 +105,6 @@ public final class Main {
       throw new IllegalStateException(builder.toString());
     }
     return plugins;
-  }
-
-  /** TODO tests. */
-  static List<String> argsForPlugin(List<String> args, WirePlugin plugin) {
-    final String argPrefix = "--" + plugin.getArgumentPrefix() + "-";
-    return FluentIterable.from(args).filter(new Predicate<String>() {
-      @Override public boolean apply(String arg) {
-        return arg.startsWith(argPrefix);
-      }
-    }).transform(new Function<String, String>() {
-      @Override public String apply(String arg) {
-        return arg.substring(argPrefix.length());
-      }
-    }).toList();
   }
 
   private Main() {
--- wire-compiler/src/main/java/com/squareup/wire/plugin/WireJavaPlugin.java
+++ wire-compiler/src/main/java/com/squareup/wire/plugin/WireJavaPlugin.java
@@ -1,15 +1,11 @@
 package com.squareup.wire.plugin;
 
 import com.squareup.protoparser.ProtoFile;
-import java.util.List;
+import java.nio.file.FileSystem;
 import java.util.Set;
 
 public class WireJavaPlugin implements WirePlugin {
-  @Override public String getArgumentPrefix() {
-    return "java";
-  }
-
-  @Override public void generate(List<String> args, Set<ProtoFile> protoFiles) {
+  @Override public void generate(FileSystem fs, Set<ProtoFile> protoFiles) {
     throw new UnsupportedOperationException("Not implemented."); // TODO
   }
 }
