bc5696e75acd6d7231a5343d5b9238f5622c8fb6
Jake Wharton <jw@squareup.com>
2015-01-16 16:59:45 -0800

    Hide non-public helper class.

--- wire-compiler/src/main/java/com/squareup/wire/OutputArtifact.java
+++ wire-compiler/src/main/java/com/squareup/wire/OutputArtifact.java
@@ -5,7 +5,7 @@ import java.io.File;
 /**
  * A container class that represents an artifact output from the compiler.
  */
-public final class OutputArtifact {
+final class OutputArtifact {
   private final String outputDirectory;
   private final String className;
   private final String javaPackage;
@@ -15,7 +15,6 @@ public final class OutputArtifact {
     this.className = className;
     this.javaPackage = javaPackage;
   }
-
 
   public String outputDirectory() {
     return outputDirectory;
