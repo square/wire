0cea57a93cf674b7692a983d495968732a95a27b
Jake Wharton <jw@squareup.com>
2015-05-07 00:55:02 -0400

    Update to Square code style and repo fluff.

--- src/main/groovy/com/squareup/wire/gradle/WireGeneratorTask.groovy
+++ src/main/groovy/com/squareup/wire/gradle/WireGeneratorTask.groovy
@@ -12,77 +12,74 @@ import org.gradle.api.tasks.TaskAction
 
 /** Task to generate Java for a set of .proto files with the Wire compiler. */
 class WireGeneratorTask extends DefaultTask {
-    Collection<WireSourceSetExtension> configurations
+  Collection<WireSourceSetExtension> configurations
 
-    @OutputDirectory
-    File outputDir
+  @OutputDirectory File outputDir
 
-    @InputFiles
-    Collection<File> getInputFiles() {
-        Set<File> inputFiles = Sets.newHashSet()
-        for (WireSourceSetExtension configuration : getConfigurations()) {
-            for (Map.Entry<String, Collection<String>> entry :
-                    configuration.getFiles().asMap().entrySet()) {
-                for (String file : entry.value) {
-                    inputFiles.add(new File(entry.key, file))
-                }
-            }
+  @InputFiles Collection<File> getInputFiles() {
+    Set<File> inputFiles = Sets.newHashSet()
+    for (WireSourceSetExtension configuration : getConfigurations()) {
+      for (Map.Entry<String, Collection<String>> entry :
+          configuration.getFiles().asMap().entrySet()) {
+        for (String file : entry.value) {
+          inputFiles.add(new File(entry.key, file))
         }
-        return inputFiles
+      }
     }
+    return inputFiles
+  }
 
-    @TaskAction
-    void generate() {
-        // Clear the output directory.
-        File outDir = getOutputDir()
-        outDir.deleteDir()
-        outDir.mkdirs()
+  @TaskAction void generate() {
+    // Clear the output directory.
+    File outDir = getOutputDir()
+    outDir.deleteDir()
+    outDir.mkdirs()
 
-        for (WireSourceSetExtension configuration : getConfigurations()) {
-            boolean noOptions = configuration.getNoOptions()
-            Collection<String> enumOptions = configuration.getEnumOptions()
-            Collection<String> roots = configuration.getRoots()
-            String serviceWriter = configuration.getServiceWriter()
-            String registryClass = configuration.getRegistryClass()
-            Collection<String> serviceWriterOpts = configuration.getServiceWriterOpts()
+    for (WireSourceSetExtension configuration : getConfigurations()) {
+      boolean noOptions = configuration.getNoOptions()
+      Collection<String> enumOptions = configuration.getEnumOptions()
+      Collection<String> roots = configuration.getRoots()
+      String serviceWriter = configuration.getServiceWriter()
+      String registryClass = configuration.getRegistryClass()
+      Collection<String> serviceWriterOpts = configuration.getServiceWriterOpts()
 
-            Multimap<String, String> files = configuration.getFiles()
-            for (Map.Entry<String, Collection<String>> entry : files.asMap().entrySet()) {
-                String protoDir = entry.key
-                List<String> args = Lists.newArrayList()
-                args.add("--proto_path=" +
-                        new File(project.projectDir, protoDir).getAbsolutePath())
+      Multimap<String, String> files = configuration.getFiles()
+      for (Map.Entry<String, Collection<String>> entry : files.asMap().entrySet()) {
+        String protoDir = entry.key
+        List<String> args = Lists.newArrayList()
+        args.add("--proto_path=" +
+            new File(project.projectDir, protoDir).getAbsolutePath())
 
-                if (noOptions) {
-                    args.add("--no_options")
-                }
-                enumOptions.each { option ->
-                    args.add("--enum_options=" + option)
-                }
-                roots.each { root ->
-                    args.add("--roots=" + root)
-                }
-                if (!Strings.isNullOrEmpty(serviceWriter)) {
-                    args.add("--service_writer=" + serviceWriter)
-                }
-                if (!Strings.isNullOrEmpty(registryClass)) {
-                    args.add("--registry_class=" + registryClass)
-                }
-                serviceWriterOpts.each { serviceWriterOpt ->
-                    args.add("--service_writer_opt=" + serviceWriterOpt)
-                }
-
-                args.add("--java_out=" + outputDir.absolutePath)
-                args.addAll(entry.value)
-
-                try {
-                    WireCompiler.main(args.toArray(new String[args.size()]))
-                } catch (Exception e) {
-                    throw new RuntimeException(
-                            "${e.getClass().getSimpleName()} generating Wire Java source for "
-                                    + "$protoDir: ${e.getMessage()}", e);
-                }
-            }
+        if (noOptions) {
+          args.add("--no_options")
         }
+        enumOptions.each { option ->
+          args.add("--enum_options=" + option)
+        }
+        roots.each { root ->
+          args.add("--roots=" + root)
+        }
+        if (!Strings.isNullOrEmpty(serviceWriter)) {
+          args.add("--service_writer=" + serviceWriter)
+        }
+        if (!Strings.isNullOrEmpty(registryClass)) {
+          args.add("--registry_class=" + registryClass)
+        }
+        serviceWriterOpts.each { serviceWriterOpt ->
+          args.add("--service_writer_opt=" + serviceWriterOpt)
+        }
+
+        args.add("--java_out=" + outputDir.absolutePath)
+        args.addAll(entry.value)
+
+        try {
+          WireCompiler.main(args.toArray(new String[args.size()]))
+        } catch (Exception e) {
+          throw new RuntimeException(
+              "${e.getClass().getSimpleName()} generating Wire Java source for "
+                  + "$protoDir: ${e.getMessage()}", e);
+        }
+      }
     }
+  }
 }
