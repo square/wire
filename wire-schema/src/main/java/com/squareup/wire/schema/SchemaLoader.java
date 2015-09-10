256aab8bca27e194ba2f358188acd4f1597e15d5
jwilson <jwilson@squareup.com>
2015-09-10 01:47:30 -0400

    Support loading proto files from ZIP files.

--- wire-schema/src/main/java/com/squareup/wire/schema/SchemaLoader.java
+++ wire-schema/src/main/java/com/squareup/wire/schema/SchemaLoader.java
@@ -30,11 +30,20 @@ import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Deque;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 import okio.Okio;
 import okio.Source;
 
-/** Load proto files and their transitive dependencies, parse them, and link them together. */
+/**
+ * Load proto files and their transitive dependencies, parse them, and link them together.
+ *
+ * <p>To find proto files to load, a non-empty set of directories is searched. Each directory is
+ * either a regular directory or a ZIP file. Within ZIP files, proto files are expected to be found
+ * relative to the root of the archive.
+ */
 public final class SchemaLoader {
   private static final Function<File, Path> FILE_TO_PATH = new Function<File, Path>() {
     @Override public Path apply(File file) {
@@ -44,6 +53,9 @@ public final class SchemaLoader {
 
   private final ImmutableList.Builder<Path> directories = ImmutableList.builder();
   private final ImmutableList.Builder<Path> protos = ImmutableList.builder();
+
+  /** Open ZIP files that need to be released. Only non-empty while loading. */
+  private final Map<Path, ZipFile> zipFiles = new LinkedHashMap<>();
 
   /** Add one or more directories from which proto files will be loaded. */
   public SchemaLoader addDirectory(File file) {
@@ -107,6 +119,32 @@ public final class SchemaLoader {
       throw new IllegalStateException("No directories added.");
     }
 
+    try {
+      // Attempt to load open each directory entry as a ZIP file. Most will fail; the rest are
+      // real ZIP files. We could ask first "is this a ZIP file" but trying and failing is easier!
+      for (Path directory : directories) {
+        if (Files.isRegularFile(directory)) {
+          try {
+            ZipFile zipFile = new ZipFile(directory.toFile());
+            zipFiles.put(directory, zipFile);
+          } catch (IOException ignored) {
+            // Not a ZIP file?!
+          }
+        }
+      }
+      return load(directories);
+    } finally {
+      for (ZipFile zipFile : zipFiles.values()) {
+        try {
+          zipFile.close();
+        } catch (IOException ignored) {
+        }
+      }
+      zipFiles.clear();
+    }
+  }
+
+  private Schema load(List<Path> directories) throws IOException {
     Deque<Path> protos = new ArrayDeque<>(this.protos.build());
     if (protos.isEmpty()) {
       // TODO traverse all files in every directory.
@@ -124,16 +162,19 @@ public final class SchemaLoader {
         if (proto.isAbsolute() && !proto.startsWith(directory)) {
           continue;
         }
-        Path resolvedPath = directory.resolve(proto);
-        if (Files.exists(resolvedPath)) {
+        Source source = source(proto, directory);
+        if (source == null) {
+          continue;
+        }
+
+        try {
           Location location = Location.get(directory.toString(), proto.toString());
-          try (Source source = Okio.source(resolvedPath)) {
-            String data = Okio.buffer(source).readUtf8();
-            element = ProtoParser.parse(location, data);
-          } catch (IOException e) {
-            throw new IOException("Failed to load " + proto, e);
-          }
-          break;
+          String data = Okio.buffer(source).readUtf8();
+          element = ProtoParser.parse(location, data);
+        } catch (IOException e) {
+          throw new IOException("Failed to load " + proto + " from " + directory, e);
+        } finally {
+          source.close();
         }
       }
       if (element == null) {
@@ -151,5 +192,20 @@ public final class SchemaLoader {
     }
 
     return new Linker(loaded.values()).link();
+  }
+
+  private Source source(Path proto, Path directory) throws IOException {
+    ZipFile zipFile = zipFiles.get(directory);
+    ZipEntry zipEntry;
+    if (zipFile != null && (zipEntry = zipFile.getEntry(proto.toString())) != null) {
+      return Okio.source(zipFile.getInputStream(zipEntry));
+    }
+
+    Path resolvedPath = directory.resolve(proto);
+    if (Files.exists(resolvedPath)) {
+      return Okio.source(resolvedPath);
+    }
+
+    return null;
   }
 }
