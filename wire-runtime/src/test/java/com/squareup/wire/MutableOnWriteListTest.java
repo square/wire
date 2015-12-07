7332c8248f092ad714978fbbbe7120a80886bf3c
Alexey Syrtsev <alexey.sircev@novardis.com>
2015-12-07 21:53:31 +0300

    UTS-133 MutableOnWriteList test update.

--- wire-runtime/src/test/java/com/squareup/wire/MutableOnWriteListTest.java
+++ wire-runtime/src/test/java/com/squareup/wire/MutableOnWriteListTest.java
@@ -23,6 +23,7 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -33,29 +34,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MutableOnWriteListTest {
 
   private List<String> initialList;
-
+  private MutableOnWriteList mutableOnWriteList;
 
   @Before
   public void init() {
-    initialList = Arrays.asList("one", "two", "three");
+    initialList = new ArrayList<>(Arrays.asList("one", "two", "three"));
+    mutableOnWriteList = new MutableOnWriteList<>(initialList);
   }
 
 
   @Test public void constructor() throws Exception {
-    // when
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
-    // then
     assertThat(Whitebox.getInternalState(mutableOnWriteList, "immutableList")).isEqualTo(initialList);
     assertThat(Whitebox.getInternalState(mutableOnWriteList, "mutableList")).isEqualTo(initialList);
   }
 
 
   @Ignore("Constructor argument parameter should be wrapped to immutable list")
-  @Test public void imutabilityViolation() throws Exception {
-    // given
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
+  @Test public void immutabilityViolation() throws Exception {
     // when
     initialList.add("four");
 
@@ -65,72 +60,49 @@ public class MutableOnWriteListTest {
   }
 
   @Test public void get() throws Exception {
-    // when
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
-    // then
     assertThat(mutableOnWriteList.get(0)).isEqualTo(initialList.get(0));
     assertThat(mutableOnWriteList.get(1)).isEqualTo(initialList.get(1));
     assertThat(mutableOnWriteList.get(2)).isEqualTo(initialList.get(2));
   }
 
   @Test public void size() throws Exception {
-    // when
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
-    // then
     assertThat(mutableOnWriteList.size()).isEqualTo(initialList.size());
   }
 
   @Test public void add() throws Exception {
-    // given
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
     // when
     mutableOnWriteList.add(0, "zero");
 
     // then
     assertThat(initialList.get(0)).isEqualTo("one");
     assertThat(mutableOnWriteList.get(0)).isEqualTo("zero");
-
     assertThat(initialList.size()).isEqualTo(3);
     assertThat(mutableOnWriteList.size()).isEqualTo(4);
   }
 
   @Test public void set() throws Exception {
-    // given
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
     // when
     mutableOnWriteList.set(0, "zero");
 
     // then
     assertThat(initialList.get(0)).isEqualTo("one");
     assertThat(mutableOnWriteList.get(0)).isEqualTo("zero");
-
     assertThat(initialList.size()).isEqualTo(3);
     assertThat(mutableOnWriteList.size()).isEqualTo(3);
   }
 
   @Test public void remove() throws Exception {
-    // given
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
     // when
     mutableOnWriteList.remove(1);
 
     // then
     assertThat(initialList.get(1)).isEqualTo("two");
     assertThat(mutableOnWriteList.get(1)).isEqualTo("three");
-
     assertThat(initialList.size()).isEqualTo(3);
     assertThat(mutableOnWriteList.size()).isEqualTo(2);
   }
 
   @Test public void repeatableRemove() throws Exception {
-    // given
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
     // when
     mutableOnWriteList.remove(0);
     mutableOnWriteList.remove(0);
@@ -138,15 +110,11 @@ public class MutableOnWriteListTest {
     // then
     assertThat(initialList.get(0)).isEqualTo("one");
     assertThat(mutableOnWriteList.get(0)).isEqualTo("three");
-
     assertThat(initialList.size()).isEqualTo(3);
     assertThat(mutableOnWriteList.size()).isEqualTo(1);
   }
 
   @Test public void writeReplace() throws Exception {
-    // given
-    MutableOnWriteList mutableOnWriteList = new MutableOnWriteList(initialList);
-
     // when
     List<String> newList = Whitebox.invokeMethod(mutableOnWriteList, "writeReplace");
     newList.remove(0);
@@ -155,10 +123,8 @@ public class MutableOnWriteListTest {
     assertThat(initialList.get(0)).isEqualTo("one");
     assertThat(mutableOnWriteList.get(0)).isEqualTo("one");
     assertThat(newList.get(0)).isEqualTo("two");
-
     assertThat(initialList.size()).isEqualTo(3);
     assertThat(mutableOnWriteList.size()).isEqualTo(3);
     assertThat(newList.size()).isEqualTo(2);
   }
-
 }
