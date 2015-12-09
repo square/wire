e69ed630c74770787dfb7f4fc45f5d9c696165de
Alexey Syrtsev <alexey.sircev@novardis.com>
2015-12-09 16:05:25 +0300

    UTS-133 ProtoReader tests in progress.

--- wire-runtime/src/test/java/com/squareup/wire/ProtoReaderTest.java
+++ wire-runtime/src/test/java/com/squareup/wire/ProtoReaderTest.java
@@ -15,18 +15,25 @@
  */
 package com.squareup.wire;
 
-import java.io.IOException;
 import okio.Buffer;
+import okio.BufferedSource;
 import okio.ByteString;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ProtocolException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.powermock.api.mockito.PowerMockito.mock;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({ ProtoReader.class })
@@ -56,6 +63,7 @@ public final class ProtoReaderTest {
   @Test public void constructor() throws Exception {
     assertThat(Whitebox.getInternalState(defaultProtoReader, "source")).isEqualTo(defaultContent);
   }
+
 
   @Test(expected = IllegalStateException.class) public void beginMessageIllegalState() throws Exception {
     // given
@@ -104,4 +112,108 @@ public final class ProtoReaderTest {
       + " reader pushed limit is not -1");
   }
 
+  @Test(expected = IOException.class) public void endMessageIllegalPos() throws Exception {
+    // given
+    Whitebox.setInternalState(defaultProtoReader, "state", 6);    // STATE_TAG
+    Whitebox.setInternalState(defaultProtoReader, "recursionDepth", 2);
+
+    // when
+    defaultProtoReader.endMessage(1);
+
+    // then
+    fail("ProtoReader should throw IOException when endMessage call is performed with invalid I/O state");
+  }
+
+  @Test(expected = IllegalStateException.class) public void nextTagIllegalState() throws Exception {
+    // when
+    defaultProtoReader.nextTag();
+
+    // then
+    fail("ProtoReader should throw IllegalStateException when beginMessage call is performed while"
+      + " reader is not in STATE_LENGTH_DELIMITED state");
+  }
+
+  @Test(expected = ProtocolException.class) public void unexpectedEndOfGroup() throws Exception {
+    // given
+    ByteString encoded = ByteString.decodeHex("130a01611c");
+    ProtoReader reader = new ProtoReader(new Buffer().write(encoded));
+
+    // when
+    reader.beginMessage();
+    reader.nextTag();
+
+    // then
+    fail("ProtoReader should throw ProtocolException when read group is not closed properly");
+  }
+
+  @Test(expected = ProtocolException.class) public void readVarint32IllegalState() throws Exception {
+    // given
+    Whitebox.setInternalState(defaultProtoReader, "state", 6);    // STATE_TAG
+
+    // when
+    defaultProtoReader.readVarint32();
+
+    // then
+    fail("ProtoReader should throw ProtocolException when readVarint32 call is performed in illegal state");
+  }
+
+  @Test(expected = ProtocolException.class) public void readVarint32MalformedValue() throws Exception {
+    // given
+    BufferedSource source = mock(BufferedSource.class);
+    PowerMockito.when(source.readByte()).thenReturn((byte)-1);
+    Whitebox.setInternalState(defaultProtoReader, "source", source);    // STATE_TAG
+
+    // when
+    defaultProtoReader.readVarint32();
+
+    // then
+    fail("ProtoReader should throw ProtocolException when readVarint32 reads malformed value");
+  }
+
+  @Test(expected = ProtocolException.class) public void readVarint64IllegalState() throws Exception {
+    // given
+    Whitebox.setInternalState(defaultProtoReader, "state", 6);    // STATE_TAG
+
+    // when
+    defaultProtoReader.readVarint64();
+
+    // then
+    fail("ProtoReader should throw ProtocolException when readVarint64 call is performed in illegal state");
+  }
+
+  @Test(expected = ProtocolException.class) public void readFixed32IllegalState() throws Exception {
+    // given
+    Whitebox.setInternalState(defaultProtoReader, "state", 6);    // STATE_TAG
+
+    // when
+    defaultProtoReader.readFixed32();
+
+    // then
+    fail("ProtoReader should throw ProtocolException when readFixed32 call is performed in illegal state");
+  }
+
+  @Test(expected = ProtocolException.class) public void readFixed64IllegalState() throws Exception {
+    // given
+    Whitebox.setInternalState(defaultProtoReader, "state", 6);    // STATE_TAG
+
+    // when
+    defaultProtoReader.readFixed64();
+
+    // then
+    fail("ProtoReader should throw ProtocolException when readFixed64 call is performed in illegal state");
+  }
+
+  private static ByteString serialize(Message message) throws Exception {
+    Buffer buffer = new Buffer();
+    ObjectOutputStream stream = new ObjectOutputStream(buffer.outputStream());
+    stream.writeObject(message);
+    stream.flush();
+    return buffer.readByteString();
+  }
+
+  public static Object deserialize(ByteString data) throws Exception {
+    Buffer buffer = new Buffer().write(data);
+    ObjectInputStream stream = new ObjectInputStream(buffer.inputStream());
+    return stream.readObject();
+  }
 }
