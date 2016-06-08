f936f174c07e297b20b02a30f5dde4d997d967a1
Jake Wharton <jw@squareup.com>
2016-06-08 19:27:00 -0400

    Add test for both --android and --compact.

--- gen_tests.sh
+++ gen_tests.sh
@@ -51,6 +51,18 @@ java -jar ../wire-compiler/target/wire-compiler-*-SNAPSHOT-jar-with-dependencies
 cp src/test/proto-java.android/com/squareup/wire/protos/person/Person.java \
    src/test/proto-java/com/squareup/wire/protos/person/Person.java.android
 
+# ANDROID COMPACT
+
+java -jar ../wire-compiler/target/wire-compiler-*-SNAPSHOT-jar-with-dependencies.jar \
+  --proto_path=../wire-runtime/src/test/proto \
+  --java_out=../wire-runtime/src/test/proto-java.android.compact \
+  --android \
+  --compact \
+  person.proto
+
+cp src/test/proto-java.android.compact/com/squareup/wire/protos/person/Person.java \
+   src/test/proto-java/com/squareup/wire/protos/person/Person.java.android.compact
+
 # COMPACT
 
 java -jar ../wire-compiler/target/wire-compiler-*-SNAPSHOT-jar-with-dependencies.jar \
@@ -73,3 +85,4 @@ rm -r src/test/proto-java.noOptions
 rm -r src/test/proto-java.pruned
 rm -r src/test/proto-java.compact
 rm -r src/test/proto-java.android
+rm -r src/test/proto-java.android.compact
