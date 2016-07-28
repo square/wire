cd2c16806c94d258a67826d1436c18e5c5cdcfa7
jwilson <jwilson@squareup.com>
2016-07-28 16:13:07 -0400

    Validate that profiles are well-formed.

--- wire-java-generator/src/main/java/com/squareup/wire/java/ProfileLoader.java
+++ wire-java-generator/src/main/java/com/squareup/wire/java/ProfileLoader.java
@@ -21,22 +21,28 @@ import com.google.common.collect.MultimapBuilder;
 import com.google.common.io.Closer;
 import com.squareup.wire.java.internal.ProfileFileElement;
 import com.squareup.wire.java.internal.ProfileParser;
+import com.squareup.wire.java.internal.TypeConfigElement;
 import com.squareup.wire.schema.Location;
 import com.squareup.wire.schema.ProtoFile;
+import com.squareup.wire.schema.ProtoType;
 import com.squareup.wire.schema.Schema;
+import com.squareup.wire.schema.SchemaException;
+import com.squareup.wire.schema.Type;
 import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import okio.Okio;
 import okio.Source;
 
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.Iterables.getOnlyElement;
 
 /**
@@ -44,28 +50,40 @@ import static com.google.common.collect.Iterables.getOnlyElement;
  * example the {@code android} profile loads {@code android.wire} files.
  */
 public final class ProfileLoader {
+  private final FileSystem fileSystem;
   private final String name;
-  private final Set<Location> protoLocations = new LinkedHashSet<>();
+  private Schema schema;
 
-  public ProfileLoader(String name) {
+  public ProfileLoader(FileSystem fileSystem, String name) {
+    this.fileSystem = fileSystem;
     this.name = name;
   }
 
-  public ProfileLoader addSchema(Schema schema) {
-    for (ProtoFile protoFile : schema.protoFiles()) {
-      protoLocations.add(protoFile.location());
-    }
-    return this;
+  public ProfileLoader(String name) {
+    this(FileSystems.getDefault(), name);
   }
 
-  public ProfileLoader addProtoLocation(Location location) {
-    protoLocations.add(location);
+  public ProfileLoader schema(Schema schema) {
+    checkState(this.schema == null);
+    this.schema = schema;
     return this;
   }
 
   public Profile load() throws IOException {
-    Multimap<Path, String> pathsToAttempt = pathsToAttempt();
+    Set<Location> protoLocations = new LinkedHashSet<>();
+    for (ProtoFile protoFile : schema.protoFiles()) {
+      protoLocations.add(protoFile.location());
+    }
+    Multimap<Path, String> pathsToAttempt = pathsToAttempt(protoLocations);
 
+    ImmutableList<ProfileFileElement> profileFiles = loadProfileFiles(pathsToAttempt);
+    Profile profile = new Profile(profileFiles);
+    validate(schema, profileFiles);
+    return profile;
+  }
+
+  private ImmutableList<ProfileFileElement> loadProfileFiles(Multimap<Path, String> pathsToAttempt)
+      throws IOException {
     ImmutableList.Builder<ProfileFileElement> result = ImmutableList.builder();
     try (Closer closer = Closer.create()) {
       for (Map.Entry<Path, Collection<String>> entry : pathsToAttempt.asMap().entrySet()) {
@@ -81,16 +99,14 @@ public final class ProfileLoader {
         }
       }
     }
-
-    // TODO(jwilson): validate type references and imports.
-    return new Profile(result.build());
+    return result.build();
   }
 
   /**
    * Returns a multimap whose keys are base directories and whose values are potential locations of
    * wire profile files.
    */
-  Multimap<Path, String> pathsToAttempt() {
+  Multimap<Path, String> pathsToAttempt(Set<Location> protoLocations) {
     Multimap<Path, String> result = MultimapBuilder.linkedHashKeys().linkedHashSetValues().build();
     for (Location location : protoLocations) {
       pathsToAttempt(result, location);
@@ -103,7 +119,7 @@ public final class ProfileLoader {
    * and adds them to {@code result}.
    */
   void pathsToAttempt(Multimap<Path, String> sink, Location location) {
-    Path base = Paths.get(location.base());
+    Path base = fileSystem.getPath(location.base());
 
     String path = location.path();
     while (!path.isEmpty()) {
@@ -130,6 +146,42 @@ public final class ProfileLoader {
     } finally {
       source.close();
     }
+  }
+
+  /** Confirms that {@code protoFiles} link correctly against {@code schema}. */
+  void validate(Schema schema, ImmutableList<ProfileFileElement> profileFiles) {
+    List<String> errors = new ArrayList<>();
+
+    for (ProfileFileElement profileFile : profileFiles) {
+      for (TypeConfigElement typeConfig : profileFile.typeConfigs()) {
+        ProtoType type = importedType(ProtoType.get(typeConfig.type()));
+        if (type == null) continue;
+
+        Type resolvedType = schema.getType(type);
+        if (resolvedType == null) {
+          errors.add(String.format("unable to resolve %s (%s)",
+              type, typeConfig.location()));
+          continue;
+        }
+
+        String requiredImport = resolvedType.location().path();
+        if (!profileFile.imports().contains(requiredImport)) {
+          errors.add(String.format("%s needs to import %s (%s)",
+              typeConfig.location().path(), requiredImport, typeConfig.location()));
+        }
+      }
+    }
+
+    if (!errors.isEmpty()) {
+      throw new SchemaException(errors);
+    }
+  }
+
+  /** Returns the type to import for {@code type}. */
+  private ProtoType importedType(ProtoType type) {
+    // Map key type is always scalar.
+    if (type.isMap()) type = type.valueType();
+    return type.isScalar() ? null : type;
   }
 
   private static Source source(Path base, String path) throws IOException {
