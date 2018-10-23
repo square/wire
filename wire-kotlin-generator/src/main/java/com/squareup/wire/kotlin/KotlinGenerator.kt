2c7fec8182ba0c2f136394b38c69d94147c660d8
Jeff Gulbronson <JGulbronson@users.noreply.github.com>
2018-10-22 21:49:40 -0400

    Add WireField annotation to Kotlin constructor parameters. (#738)
    * Add WireField annotation to Kotlin constructor parameters.
    
    * Move @WireField annotation behind Java interop flag.

--- wire-kotlin-generator/src/main/java/com/squareup/wire/kotlin/KotlinGenerator.kt
+++ wire-kotlin-generator/src/main/java/com/squareup/wire/kotlin/KotlinGenerator.kt
@@ -44,6 +44,7 @@ import com.squareup.wire.ProtoAdapter
 import com.squareup.wire.ProtoReader
 import com.squareup.wire.ProtoWriter
 import com.squareup.wire.WireEnum
+import com.squareup.wire.WireField
 import com.squareup.wire.internal.Internal
 import com.squareup.wire.schema.EnumType
 import com.squareup.wire.schema.Field
@@ -307,6 +308,13 @@ class KotlinGenerator private constructor(
       val parameterSpec = ParameterSpec.builder(fieldName, fieldClass)
       if (field.isOptional || field.isRepeated) {
         parameterSpec.defaultValue(defaultValue)
+      }
+
+      if (javaInterOp) {
+        parameterSpec.addAnnotation(AnnotationSpec.builder(WireField::class)
+            .addMember("tag = %L", field.tag())
+            .addMember("adapter = %S", getAdapterName(field))
+            .build())
       }
 
       constructorBuilder.addParameter(parameterSpec.build())
