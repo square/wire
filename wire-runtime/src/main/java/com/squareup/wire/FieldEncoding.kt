1ad4284e82e0de3bb227385e14d6c840921983ff
Egor Andreevich <andreevich.egor@gmail.com>
2019-04-14 18:06:22 -0400

    Convert FieldEncoding.java to Kotlin (#906)
    * Convert FieldEncoding.java to Kotlin
    
    * Rename FieldEncoding.java to .kt
    
    * Add JvmField to value
    
    * Update Japicmp excludes

---
+++ wire-runtime/src/main/java/com/squareup/wire/FieldEncoding.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2013 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.wire
+
+import java.io.IOException
+import java.net.ProtocolException
+
+// TODO(egor): Remove JvmField once ProtoWriter is in Kotlin
+enum class FieldEncoding(@JvmField internal val value: Int) {
+  VARINT(0), FIXED64(1), LENGTH_DELIMITED(2), FIXED32(5);
+
+  /**
+   * Returns a Wire adapter that reads this field encoding without interpretation. For example,
+   * messages are returned as byte strings and enums are returned as integers.
+   */
+  fun rawProtoAdapter(): ProtoAdapter<*> = when (this) {
+    VARINT -> ProtoAdapter.UINT64
+    FIXED32 -> ProtoAdapter.FIXED32
+    FIXED64 -> ProtoAdapter.FIXED64
+    LENGTH_DELIMITED -> ProtoAdapter.BYTES
+  }
+
+  companion object {
+    @JvmStatic
+    @Throws(IOException::class)
+    internal operator fun get(value: Int): FieldEncoding = when (value) {
+      0 -> VARINT
+      1 -> FIXED64
+      2 -> LENGTH_DELIMITED
+      5 -> FIXED32
+      else -> throw ProtocolException("Unexpected FieldEncoding: $value")
+    }
+  }
+}
