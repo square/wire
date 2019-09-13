fac30b78a4c46fa6f02657f371c10fbbfb5d9379
Egor Andreevici <egor@squareup.com>
2019-09-13 13:47:36 -0400

    Add tests

---
+++ wire-runtime/src/commonTest/kotlin/com/squareup/wire/ProtoAdapterTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.wire
+
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class ProtoAdapterTest {
+  @Test fun repeatedRepeatedProtoAdapterForbidden() {
+    assertFailsWith(UnsupportedOperationException::class) {
+      ProtoAdapter.BOOL.asRepeated().asRepeated()
+    }
+  }
+
+  @Test fun packedPackedProtoAdapterForbidden() {
+    // Unable to pack a length-delimited type.
+    assertFailsWith(IllegalArgumentException::class) {
+      ProtoAdapter.BOOL.asPacked().asPacked()
+    }
+  }
+
+  @Test fun repeatedPackedProtoAdapterForbidden() {
+    assertFailsWith(UnsupportedOperationException::class) {
+      ProtoAdapter.BOOL.asRepeated().asPacked()
+    }
+  }
+
+  @Test fun packedRepeatedProtoAdapterForbidden() {
+    assertFailsWith(UnsupportedOperationException::class) {
+      ProtoAdapter.BOOL.asPacked().asRepeated()
+    }
+  }
+}
