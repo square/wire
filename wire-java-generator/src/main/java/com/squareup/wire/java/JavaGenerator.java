85de89f0f88f457bbaf8819ff8ac5765064b105e
Marco Ellwanger <marco.ellwanger@hotschedules.com>
2019-12-16 17:15:23 -0800

    Prevent class/variable name clash during compilation of generated sources when a field has the same name as a type within the schema

--- wire-java-generator/src/main/java/com/squareup/wire/java/JavaGenerator.java
+++ wire-java-generator/src/main/java/com/squareup/wire/java/JavaGenerator.java
@@ -164,7 +164,7 @@ public final class JavaGenerator {
         Set<String> collidingNames = collidingFieldNames(fieldsAndOneOfFields);
         for (Field field : fieldsAndOneOfFields) {
           String suggestion = collidingNames.contains(field.getName())
-            || schema.getType(field.getPackageName() + "." + field.getName()) != null
+            || schema.getType(field.getQualifiedName()) != null
             || field.getName().equals(field.getType().getSimpleName())
               ? field.getQualifiedName()
               : field.getName();
