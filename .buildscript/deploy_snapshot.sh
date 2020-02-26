749480bd9b5619fdae3be9868f5e4dfa46f7c313
Benoit Quenaudon <bquenaudon@squareup.com>
2020-02-26 10:01:50 -0500

    Update travis/deploy_snapshot script

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -18,6 +18,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying snapshot..."
-  ./gradlew clean uploadArchives publish
+  cd wire-library; ../gradlew clean uploadArchives publish
   echo "Snapshot deployed!"
 fi
