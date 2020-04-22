9a3ce13dbb75197a072b2535113d3141280849e1
Benoit Quenaudon <bquenaudon@squareup.com>
2020-04-22 15:24:40 -0400

    Use name allocation for missingRequiredFields params

--- wire-library/wire-moshi-adapter/src/test/java/squareup/keywords/KeywordJava.java
+++ wire-library/wire-moshi-adapter/src/test/java/squareup/keywords/KeywordJava.java
@@ -15,6 +15,7 @@ import java.lang.Object;
 import java.lang.Override;
 import java.lang.String;
 import java.lang.StringBuilder;
+import java.util.List;
 import java.util.Map;
 import okio.ByteString;
 
@@ -37,6 +38,7 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
   @WireField(
       tag = 2,
       adapter = "com.squareup.wire.ProtoAdapter#BOOL",
+      label = WireField.Label.REQUIRED,
       declaredName = "public"
   )
   public final Boolean public_;
@@ -49,16 +51,26 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
   )
   public final Map<String, String> package_;
 
-  public KeywordJava(String final_, Boolean public_, Map<String, String> package_) {
-    this(final_, public_, package_, ByteString.EMPTY);
+  @WireField(
+      tag = 4,
+      adapter = "com.squareup.wire.ProtoAdapter#BOOL",
+      label = WireField.Label.REPEATED,
+      declaredName = "return"
+  )
+  public final List<Boolean> return_;
+
+  public KeywordJava(String final_, Boolean public_, Map<String, String> package_,
+      List<Boolean> return_) {
+    this(final_, public_, package_, return_, ByteString.EMPTY);
   }
 
   public KeywordJava(String final_, Boolean public_, Map<String, String> package_,
-      ByteString unknownFields) {
+      List<Boolean> return_, ByteString unknownFields) {
     super(ADAPTER, unknownFields);
     this.final_ = final_;
     this.public_ = public_;
     this.package_ = Internal.immutableCopyOf("package_", package_);
+    this.return_ = Internal.immutableCopyOf("return_", return_);
   }
 
   @Override
@@ -67,6 +79,7 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
     builder.final_ = final_;
     builder.public_ = public_;
     builder.package_ = Internal.copyOf(package_);
+    builder.return_ = Internal.copyOf(return_);
     builder.addUnknownFields(unknownFields());
     return builder;
   }
@@ -78,8 +91,9 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
     KeywordJava o = (KeywordJava) other;
     return unknownFields().equals(o.unknownFields())
         && Internal.equals(final_, o.final_)
-        && Internal.equals(public_, o.public_)
-        && package_.equals(o.package_);
+        && public_.equals(o.public_)
+        && package_.equals(o.package_)
+        && return_.equals(o.return_);
   }
 
   @Override
@@ -88,8 +102,9 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
     if (result == 0) {
       result = unknownFields().hashCode();
       result = result * 37 + (final_ != null ? final_.hashCode() : 0);
-      result = result * 37 + (public_ != null ? public_.hashCode() : 0);
+      result = result * 37 + public_.hashCode();
       result = result * 37 + package_.hashCode();
+      result = result * 37 + return_.hashCode();
       super.hashCode = result;
     }
     return result;
@@ -99,8 +114,9 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
   public String toString() {
     StringBuilder builder = new StringBuilder();
     if (final_ != null) builder.append(", final=").append(Internal.sanitize(final_));
-    if (public_ != null) builder.append(", public=").append(public_);
+    builder.append(", public=").append(public_);
     if (!package_.isEmpty()) builder.append(", package=").append(package_);
+    if (!return_.isEmpty()) builder.append(", return=").append(return_);
     return builder.replace(0, 2, "KeywordJava{").append('}').toString();
   }
 
@@ -111,8 +127,11 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
 
     public Map<String, String> package_;
 
+    public List<Boolean> return_;
+
     public Builder() {
       package_ = Internal.newMutableMap();
+      return_ = Internal.newMutableList();
     }
 
     public Builder final_(String final_) {
@@ -131,9 +150,18 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
       return this;
     }
 
+    public Builder return_(List<Boolean> return_) {
+      Internal.checkElementsNotNull(return_);
+      this.return_ = return_;
+      return this;
+    }
+
     @Override
     public KeywordJava build() {
-      return new KeywordJava(final_, public_, package_, super.buildUnknownFields());
+      if (public_ == null) {
+        throw Internal.missingRequiredFields(public_, "public");
+      }
+      return new KeywordJava(final_, public_, package_, return_, super.buildUnknownFields());
     }
   }
 
@@ -149,6 +177,7 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
       return ProtoAdapter.STRING.encodedSizeWithTag(1, value.final_)
           + ProtoAdapter.BOOL.encodedSizeWithTag(2, value.public_)
           + package_.encodedSizeWithTag(3, value.package_)
+          + ProtoAdapter.BOOL.asRepeated().encodedSizeWithTag(4, value.return_)
           + value.unknownFields().size();
     }
 
@@ -157,6 +186,7 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
       ProtoAdapter.STRING.encodeWithTag(writer, 1, value.final_);
       ProtoAdapter.BOOL.encodeWithTag(writer, 2, value.public_);
       package_.encodeWithTag(writer, 3, value.package_);
+      ProtoAdapter.BOOL.asRepeated().encodeWithTag(writer, 4, value.return_);
       writer.writeBytes(value.unknownFields());
     }
 
@@ -169,6 +199,7 @@ public final class KeywordJava extends Message<KeywordJava, KeywordJava.Builder>
           case 1: builder.final_(ProtoAdapter.STRING.decode(reader)); break;
           case 2: builder.public_(ProtoAdapter.BOOL.decode(reader)); break;
           case 3: builder.package_.putAll(package_.decode(reader)); break;
+          case 4: builder.return_.add(ProtoAdapter.BOOL.decode(reader)); break;
           default: {
             reader.readUnknownField(tag);
           }
