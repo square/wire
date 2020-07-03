d3e82316c5622b2460b0d95a0c6ff9e330801477
Benoit Quenaudon <bquenaudon@squareup.com>
2020-07-03 16:48:01 -0400

    Create module for json Kotlin tests

--- wire-library/wire-tests/src/jvmJsonTest/kotlin/com/squareup/wire/WireJsonTest.kt
+++ wire-library/wire-tests/src/jvmJsonTest/kotlin/com/squareup/wire/WireJsonTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Square Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.squareup.wire;
 
 import com.google.gson.GsonBuilder
@@ -15,9 +30,12 @@ import org.junit.runners.Parameterized.Parameters
 import java.io.File
 import java.util.Collections
 
-/** Tests meant for Java generated code to be executed among different JSON libraries. */
+/**
+ * Tests meant to be executed against both Java generated and Kotlin generated code among different
+ * JSON libraries.
+ */
 @RunWith(Parameterized::class)
-class WireJsonJavaTest {
+class WireJsonTest {
   @Parameterized.Parameter(0)
   internal lateinit var jsonLibrary: JsonLibrary
 
