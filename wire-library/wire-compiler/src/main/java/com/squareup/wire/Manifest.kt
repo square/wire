c8cd79762e83b082d649a592a0518bbbff9e9aef
Jake Wharton <jw@squareup.com>
2020-07-24 16:54:08 -0400

    Extract schema partitioning by manifest to be language-agnostic
    The manifest will partition a schema into a dedicated paritioned schema value type. That value type can then be used with a SchemaHandler factory to create an uber SchemaHandler which will produce the partitioned modules.
    
    Eventually we want to extract this above WireRun and targets, but that will come later.

--- wire-library/wire-compiler/src/main/java/com/squareup/wire/Manifest.kt
+++ wire-library/wire-compiler/src/main/java/com/squareup/wire/Manifest.kt
@@ -16,14 +16,62 @@
 package com.squareup.wire
 
 import com.charleskorn.kaml.Yaml
+import com.squareup.wire.PartitionedSchema.Partition
+import com.squareup.wire.schema.ProtoType
+import com.squareup.wire.schema.PruningRules
+import com.squareup.wire.schema.Schema
+import com.squareup.wire.schema.Service
+import com.squareup.wire.schema.Target.SchemaHandler
+import com.squareup.wire.schema.Type
 import com.squareup.wire.schema.internal.DagChecker
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.MapSerializer
 import kotlinx.serialization.builtins.serializer
+import java.nio.file.Path
+import java.util.ArrayDeque
 
 data class Manifest(
   val modules: Map<String, Module>
 ) {
+  init {
+    val dagChecker = DagChecker(modules.keys) { moduleName ->
+      modules.getValue(moduleName).dependencies
+    }
+    val cycles = dagChecker.check()
+    require(cycles.isEmpty()) {
+      buildString {
+        append("ERROR: Manifest modules contain dependency cycles:\n")
+        for (cycle in cycles) {
+          append(" - ")
+          append(cycle)
+          append('\n')
+        }
+      }
+    }
+  }
+
+  /** Modules which have no dependencies. */
+  val roots: Set<String> = modules.filterValues { it.dependencies.isEmpty() }.keys
+
+  /** Module ordering such that every module will see its dependencies appear before it. */
+  val order: List<String> = run {
+    val seen = LinkedHashSet<String>() // Insertion order is important to produce the final list!
+    val queue = ArrayDeque<String>().apply { addAll(roots) }
+    while (queue.isNotEmpty()) {
+      val currentName = queue.removeFirst()
+      val currentDependencies = modules.getValue(currentName).dependencies
+      if (seen.containsAll(currentDependencies)) {
+        seen += currentName
+
+        val downstreamModules = modules.filterValues { currentName in it.dependencies }.keys
+        queue += downstreamModules
+      } else {
+        queue += currentName
+      }
+    }
+    seen.toList()
+  }
+
   @Serializable
   data class Module(
     val dependencies: Set<String> = emptySet(),
@@ -40,24 +88,222 @@ data class Manifest(
     private val serializer = MapSerializer(String.serializer(), Module.serializer())
 
     fun fromYaml(string: String): Manifest {
-      val modules = Yaml.default.parse(serializer, string)
+      return Manifest(Yaml.default.parse(serializer, string))
+    }
+  }
+}
 
-      val dagChecker = DagChecker(modules.keys) { moduleName ->
-        modules.getValue(moduleName).dependencies
+class PartitionedSchema(
+  /** Module name to partition info. The iteration order of this map is the generation order. */
+  val modules: Map<String, Partition>,
+  val warnings: List<String>,
+  val errors: List<String>
+) {
+  class Partition(
+    val schema: Schema,
+    val types: Set<ProtoType>,
+    val upstreamTypes: Map<ProtoType, String>
+  )
+}
+
+private const val debug = false
+
+fun Schema.partition(manifest: Manifest): PartitionedSchema {
+  val modules = manifest.modules
+
+  if (debug) {
+    println("Modules: ${modules.keys}")
+    println("Roots: ${manifest.roots}")
+    println("Order: ${manifest.order}")
+  }
+
+  val disjointGraphs = manifest.roots
+      .map { root ->
+        val reachableNames = mutableSetOf<String>()
+        val visitQueue = ArrayDeque<String>().apply { add(root) }
+        while (visitQueue.isNotEmpty()) {
+          val visitName = visitQueue.removeFirst()
+          reachableNames += visitName
+
+          val upstreamModules = modules.getValue(visitName).dependencies
+          val downstreamModules = modules.filterValues { visitName in it.dependencies }.keys
+          for (dependency in upstreamModules + downstreamModules) {
+            if (dependency !in reachableNames && dependency !in visitQueue) {
+              visitQueue += dependency
+            }
+          }
+        }
+        // Sort the reachable names by their module order.
+        manifest.order.filter { it in reachableNames }
       }
-      val cycles = dagChecker.check()
-      require(cycles.isEmpty()) {
-        buildString {
-          append("ERROR: Manifest modules contain dependency cycles:\n")
-          for (cycle in cycles) {
-            append(" - ")
-            append(cycle)
-            append('\n')
+      .toSet()
+  if (debug) {
+    println("Disjoint Graphs:")
+    for (disjointGraph in disjointGraphs) {
+      println(" - $disjointGraph")
+    }
+  }
+
+  val errors = mutableListOf<String>()
+  val partitions = mutableMapOf<String, Partition>()
+  for (moduleName in manifest.order) {
+    val module = modules.getValue(moduleName)
+
+    val transitiveDependencySet = mutableSetOf<String>().apply {
+      val queue = ArrayDeque<String>().apply { add(moduleName) }
+      while (queue.isNotEmpty()) {
+        val dependency = modules.getValue(queue.removeFirst())
+        addAll(dependency.dependencies)
+        queue.addAll(dependency.dependencies)
+      }
+    } as Set<String> // TODO use buildSet
+
+    val pruningRules = PruningRules.Builder()
+        .apply {
+          for (dependencyName in transitiveDependencySet) {
+            val dependency = modules.getValue(dependencyName)
+            addRoot(dependency.roots)
+            prune(dependency.prunes)
+          }
+          addRoot(module.roots)
+          // TODO(jw): what do we do about prunes that apply to types in an upstream module? fail?
+          prune(module.prunes)
+        }
+        .build()
+
+    val upstreamTypes = mutableMapOf<ProtoType, String>().apply {
+      val duplicateTypes = mutableMapOf<ProtoType, MutableSet<String>>()
+      for (dependencyName in transitiveDependencySet) {
+        for (type in partitions.getValue(dependencyName).types) {
+          val replaced = put(type, dependencyName)
+          if (replaced != null) {
+            duplicateTypes.getOrPut(type) { mutableSetOf(replaced) }.add(dependencyName)
           }
         }
       }
+      for ((duplicate, sourceModules) in duplicateTypes) {
+        errors += """
+          |$moduleName sees $duplicate in ${sourceModules.joinToString()}.
+          |  In order to avoid confusion and incompatibility, this type should be moved up into a
+          |  common dependency of the offending modules.
+          """.trimMargin()
+      }
+    } as Map<ProtoType, String> // TODO use buildMap
 
-      return Manifest(modules)
+    val moduleSchema = prune(pruningRules)
+
+    if (debug) {
+      fun Schema.stats() = buildString {
+        append(protoFiles.size)
+        append(" files, ")
+        append(protoFiles.sumBy { it.typesAndNestedTypes().size })
+        append(" types, ")
+        append(protoFiles.sumBy { it.services.size })
+        append(" services")
+      }
+      println()
+      println(moduleName)
+      println("  Dependencies: ${module.dependencies}")
+      println("  Rules:")
+      println("   - roots: ${module.roots}")
+      println("   - prunes: ${module.prunes}")
+      println("  Schema:")
+      println("   - original: ${stats()}")
+      println("   - pruned: ${moduleSchema.stats()}")
+      println("  Existing: ${upstreamTypes.size} proto types")
+      println("  Content:")
+    }
+    val generatedTypes = mutableSetOf<ProtoType>()
+    for (protoFile in moduleSchema.protoFiles) {
+      for (type in protoFile.typesAndNestedTypes()) {
+        val protoType = type.type
+        if (protoType !in upstreamTypes) {
+          if (debug) {
+            println("   - type: $protoType")
+          }
+          generatedTypes += protoType
+        }
+      }
+      for (service in protoFile.services) {
+        val protoType = service.type()
+        if (protoType !in upstreamTypes) {
+          if (debug) {
+            println("   - service: $protoType")
+          }
+          generatedTypes += protoType
+        }
+      }
+
+      partitions[moduleName] = Partition(moduleSchema, generatedTypes, upstreamTypes)
+    }
+  }
+
+  val warnings = mutableListOf<String>()
+  for (moduleGraph in disjointGraphs) {
+    for (currentName in moduleGraph) {
+      for (otherName in moduleGraph) {
+        if (otherName == currentName) {
+          break
+        }
+        val currentTypes = partitions.getValue(currentName).types
+        val otherTypes = partitions.getValue(otherName).types
+        val duplicates = currentTypes.intersect(otherTypes)
+        if (duplicates.isNotEmpty()) {
+          val currentModule = modules.getValue(currentName)
+          val otherModule = modules.getValue(otherName)
+          for (duplicate in duplicates) {
+            val duplicateName = duplicate.toString()
+            if (duplicateName !in currentModule.roots || duplicateName !in otherModule.roots) {
+              warnings += """
+                |$duplicate is generated twice in peer modules $currentName and $otherName.
+                |  Consider moving this type into a common dependency of both modules.
+                |  To suppress this warning, explicitly add the type to the roots of both modules.
+                """.trimMargin()
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return PartitionedSchema(partitions, warnings, errors)
+}
+
+fun PartitionedSchema.buildSchemaHandler(
+  factory: (name: String, partition: Partition) -> SchemaHandler
+): SchemaHandler {
+  // TODO this means you cannot generate the same type in two unrelated modules! fix!
+  val handlers = modules
+      .map { (name, partition) ->
+        // Create the handler for each partition.
+        partition to factory(name, partition)
+      }
+      .flatMap { partitionHandlerPair ->
+        // Associate each partition type with the partition/handler pair.
+        partitionHandlerPair.first.types.map { it to partitionHandlerPair }
+      }
+      .toMap()
+
+  return object : SchemaHandler {
+    override fun handle(type: Type): Path? {
+      val protoType = type.type
+      return handlers[protoType]?.let { (partition, delegate) ->
+        // The schema used to invoke this function is the global one which has not been pruned so we
+        // swap its type for the corresponding pruned one.
+        // TODO(jw): Split WireRun into two phases so we can insert our pruned schema before step 4.
+        val prunedType = partition.schema.getType(protoType)!!
+        delegate.handle(prunedType)
+      }
+    }
+    override fun handle(service: Service): List<Path> {
+      val protoType = service.type()
+      return handlers[protoType]?.let { (partition, delegate) ->
+        // The schema used to invoke this function is the global one which has not been pruned so we
+        // swap its type for the corresponding pruned one.
+        // TODO(jw): Split WireRun into two phases so we can insert our pruned schema before step 4.
+        val prunedType = partition.schema.getService(protoType)!!
+        delegate.handle(prunedType)
+      } ?: emptyList()
     }
   }
 }
--- wire-library/wire-compiler/src/main/java/com/squareup/wire/schema/Target.kt
+++ wire-library/wire-compiler/src/main/java/com/squareup/wire/schema/Target.kt
@@ -27,8 +27,10 @@ import com.squareup.wire.java.Profile
 import com.squareup.wire.kotlin.KotlinGenerator
 import com.squareup.wire.kotlin.RpcCallStyle
 import com.squareup.wire.kotlin.RpcRole
+import com.squareup.wire.partition
 import com.squareup.wire.schema.Target.SchemaHandler
 import com.squareup.wire.swift.SwiftGenerator
+import com.squareup.wire.buildSchemaHandler
 import okio.buffer
 import okio.sink
 import java.io.IOException
@@ -36,7 +38,6 @@ import java.io.Serializable
 import java.nio.file.FileSystem
 import java.nio.file.Files
 import java.nio.file.Path
-import java.util.ArrayDeque
 import io.outfoxx.swiftpoet.FileSpec as SwiftFileSpec
 
 sealed class Target : Serializable {
@@ -306,8 +307,7 @@ data class SwiftTarget(
   override val excludes: List<String> = listOf(),
   override val exclusive: Boolean = true,
   val outDirectory: String,
-  val manifest: Manifest,
-  val debug: Boolean = false
+  val manifest: Manifest
 ) : Target() {
   override fun newHandler(
     schema: Schema,
@@ -315,248 +315,50 @@ data class SwiftTarget(
     logger: WireLogger,
     newProfileLoader: NewProfileLoader
   ): SchemaHandler {
+    val partitionedSchema = schema.partition(manifest)
+
+    for (warning in partitionedSchema.warnings) {
+      println("WARNING: $warning")
+    }
+    if (partitionedSchema.errors.isNotEmpty()) {
+      for (error in partitionedSchema.errors) {
+        System.err.println("ERROR: $error")
+      }
+      throw IllegalStateException("Manifest produced partitioning errors. See output above.")
+    }
+
     val outputRoot = fs.getPath(outDirectory)
-
-    val modules = manifest.modules
-    val moduleRoots = modules.filterValues { it.dependencies.isEmpty() }.keys
-    val moduleOrder = run {
-      val seen = LinkedHashSet<String>() // Insertion order is important to produce the final list!
-      val queue = ArrayDeque<String>().apply { addAll(moduleRoots) }
-      while (queue.isNotEmpty()) {
-        val currentName = queue.removeFirst()
-        val currentDependencies = modules.getValue(currentName).dependencies
-        if (seen.containsAll(currentDependencies)) {
-          seen += currentName
-
-          val downstreamModules = modules.filterValues { currentName in it.dependencies }.keys
-          queue += downstreamModules
-        } else {
-          queue += currentName
-        }
-      }
-      seen.toList()
-    }
-    if (debug) {
-      println("Modules: ${modules.keys}")
-      println("Roots: $moduleRoots")
-      println("Order: $moduleOrder")
-    }
-
-    val disjointGraphs = moduleRoots
-        .map { root ->
-          val reachableNames = mutableSetOf<String>()
-          val visitQueue = ArrayDeque<String>().apply { add(root) }
-          while (visitQueue.isNotEmpty()) {
-            val visitName = visitQueue.removeFirst()
-            reachableNames += visitName
-
-            val upstreamModules = modules.getValue(visitName).dependencies
-            val downstreamModules = modules.filterValues { visitName in it.dependencies }.keys
-            for (dependency in upstreamModules + downstreamModules) {
-              if (dependency !in reachableNames && dependency !in visitQueue) {
-                visitQueue += dependency
-              }
-            }
-          }
-          // Sort the reachable names by their module order.
-          moduleOrder.filter { it in reachableNames }
-        }
-        .toSet()
-    if (debug) {
-      println("Disjoint Graphs:")
-      for (disjointGraph in disjointGraphs) {
-        println(" - $disjointGraph")
-      }
-    }
-
-    // TODO this means you cannot generate the same type in two unrelated modules! fix!
-    val typeHandlers = mutableMapOf<ProtoType, SchemaHandler>()
-
-    /** The types generated directly in each module (i.e., not including transitives). */
-    val moduleTypes = mutableMapOf<String, Set<ProtoType>>()
-
-    for (moduleName in moduleOrder) {
-      val module = modules.getValue(moduleName)
-
-      val transitiveDependencySet = mutableSetOf<String>().apply {
-        val queue = ArrayDeque<String>().apply { add(moduleName) }
-        while (queue.isNotEmpty()) {
-          val dependency = modules.getValue(queue.removeFirst())
-          addAll(dependency.dependencies)
-          queue.addAll(dependency.dependencies)
-        }
-      } as Set<String> // TODO use buildSet
-
-      val destination = outputRoot.resolve(moduleName)
+    return partitionedSchema.buildSchemaHandler { module, partition ->
+      val generator = SwiftGenerator(partition.schema, partition.upstreamTypes)
+      val destination = outputRoot.resolve(module)
       Files.createDirectories(destination) // TODO upstream to SwiftPoet's writeTo.
 
-      val pruningRules = PruningRules.Builder()
-          .apply {
-            for (dependencyName in transitiveDependencySet) {
-              val dependency = modules.getValue(dependencyName)
-              addRoot(dependency.roots)
-              prune(dependency.prunes)
-            }
-            addRoot(module.roots)
-            // TODO(jw): what do we do about prunes that apply to types in an upstream module? fail?
-            prune(module.prunes)
-          }
-          .build()
-
-      val existingTypes = mutableMapOf<ProtoType, String>().apply {
-        val duplicateTypes = mutableMapOf<ProtoType, MutableSet<String>>()
-        for (dependencyName in transitiveDependencySet) {
-          for (type in moduleTypes.getValue(dependencyName)) {
-            val replaced = put(type, dependencyName)
-            if (replaced != null) {
-              duplicateTypes.getOrPut(type) { mutableSetOf(replaced) }.add(dependencyName)
-            }
-          }
-        }
-        check(duplicateTypes.isEmpty()) {
-          buildString {
-            append("Module '$moduleName' sees duplicate types in its dependencies:\n\n")
-            for ((type, sourceModules) in duplicateTypes) {
-              append(" - $type in $sourceModules\n")
-            }
-            append("\nIn order to avoid confusion, each type should be moved up into a dependency ")
-            append("which is common to the modules listed for that type.")
-          }
-        }
-      } as Map<ProtoType, String> // TODO use buildMap
-
-      val moduleSchema = schema.prune(pruningRules)
-      val generator = SwiftGenerator(moduleSchema, existingTypes)
-      val moduleGenerator = ModuleSchemaHandler(generator, destination, logger)
-
-      if (debug) {
-        fun Schema.stats() = buildString {
-          append(protoFiles.size)
-          append(" files, ")
-          append(protoFiles.sumBy { it.typesAndNestedTypes().size })
-          append(" types, ")
-          append(protoFiles.sumBy { it.services.size })
-          append(" services")
-        }
-        println()
-        println(moduleName)
-        println("  Destination: $destination")
-        println("  Dependencies: ${module.dependencies}")
-        println("  Rules:")
-        println("   - roots: ${module.roots}")
-        println("   - prunes: ${module.prunes}")
-        println("  Schema:")
-        println("   - original: ${schema.stats()}")
-        println("   - pruned: ${moduleSchema.stats()}")
-        println("  Existing: ${existingTypes.size} proto types")
-        println("  Content:")
-      }
-      val generatedTypes = mutableSetOf<ProtoType>()
-      for (protoFile in moduleSchema.protoFiles) {
-        for (type in protoFile.typesAndNestedTypes()) {
-          val protoType = type.type
-          if (protoType !in existingTypes) {
-            if (debug) {
-              println("   - type: $protoType")
-            }
-            generatedTypes += protoType
-            typeHandlers[protoType] = moduleGenerator
-          }
-        }
-        for (service in protoFile.services) {
-          val protoType = service.type()
-          if (protoType !in existingTypes) {
-            if (debug) {
-              println("   - service: $protoType")
-            }
-            generatedTypes += protoType
-            typeHandlers[protoType] = moduleGenerator
-          }
-        }
-
-        moduleTypes[moduleName] = generatedTypes
-      }
-    }
-
-    val warnings = mutableListOf<String>()
-    for (moduleGraph in disjointGraphs) {
-      for (currentName in moduleGraph) {
-        for (otherName in moduleGraph) {
-          if (otherName == currentName) {
-            break
-          }
-          val currentTypes = moduleTypes.getValue(currentName)
-          val otherTypes = moduleTypes.getValue(otherName)
-          val duplicates = currentTypes.intersect(otherTypes)
-          if (duplicates.isNotEmpty()) {
-            val currentModule = modules.getValue(currentName)
-            val otherModule = modules.getValue(otherName)
-            for (duplicate in duplicates) {
-              val duplicateName = duplicate.toString()
-              if (duplicateName !in currentModule.roots || duplicateName !in otherModule.roots) {
-                warnings += "$duplicate generated in both $currentName and $otherName"
+      object : SchemaHandler {
+        override fun handle(type: Type): Path? {
+          val typeName = generator.generatedTypeName(type)
+          val swiftFile = SwiftFileSpec.builder(typeName.moduleName, typeName.simpleName)
+              .addComment(WireCompiler.CODE_GENERATED_BY_WIRE)
+              .addComment("\nSource: %L in %L", type.type, type.location.withPathOnly())
+              .indent("    ")
+              .apply {
+                generator.generateTypeTo(type, this)
               }
-            }
+              .build()
+
+          try {
+            swiftFile.writeTo(destination)
+          } catch (e: IOException) {
+            throw IOException(
+                "Error emitting ${swiftFile.moduleName}.${typeName.canonicalName} to $destination", e)
           }
+
+          logger.artifact(destination, type.type, swiftFile)
+          return destination.resolve("${swiftFile.name}.swift")
         }
+
+        override fun handle(service: Service) = emptyList<Path>()
       }
     }
-    if (warnings.isNotEmpty()) {
-      println()
-      println("WARNING: Duplicate types generated in peer modules")
-      println()
-      for (warning in warnings) {
-        println(" - $warning")
-      }
-      println()
-      println("Consider moving these types into a common dependency of the listed modules.")
-      println("To suppress this warning, explicitly add the type to the roots of both modules.")
-      println()
-    }
-
-    return object : SchemaHandler {
-      override fun handle(type: Type): Path? {
-        return typeHandlers[type.type]?.handle(type)
-      }
-      override fun handle(service: Service): List<Path> {
-        return typeHandlers[service.type()]?.handle(service) ?: emptyList()
-      }
-    }
-  }
-
-  private class ModuleSchemaHandler(
-    private val generator: SwiftGenerator,
-    private val destination: Path,
-    private val logger: WireLogger
-  ) : SchemaHandler {
-    override fun handle(unprunedType: Type): Path {
-      // The schema used to invoke this function is the global one which has not been pruned so we
-      // swap its type for the corresponding pruned one.
-      // TODO(jw): Split WireRun into two phases so we can insert our pruned schema before step 4.
-      val type = generator.schema.getType(unprunedType.type)!!
-
-      val typeName = generator.generatedTypeName(type)
-      val swiftFile = SwiftFileSpec.builder(typeName.moduleName, typeName.simpleName)
-          .addComment(WireCompiler.CODE_GENERATED_BY_WIRE)
-          .addComment("\nSource: %L in %L", type.type, type.location.withPathOnly())
-          .indent("    ")
-          .apply {
-            generator.generateTypeTo(type, this)
-          }
-          .build()
-
-      try {
-        swiftFile.writeTo(destination)
-      } catch (e: IOException) {
-        throw IOException(
-            "Error emitting ${swiftFile.moduleName}.${typeName.canonicalName} to $destination", e)
-      }
-
-      logger.artifact(destination, type.type, swiftFile)
-      return destination.resolve("${swiftFile.name}.swift")
-    }
-
-    override fun handle(service: Service) = emptyList<Path>() // TODO not supported
   }
 }
 
