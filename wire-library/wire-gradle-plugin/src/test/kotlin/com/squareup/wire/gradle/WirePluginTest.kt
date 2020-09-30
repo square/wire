ef1e11e537b182f920b1ed713bfa8ebb017da4c4
Benoît Quenaudon <bquenaudon@squareup.com>
2020-09-30 11:35:51 -0400

    WirePlugin Improvements (#1799)
    * WirePlugin Improvements
    
    New: creates a new generation task per source (main, variants, etc)
    New: Adds wire-runtime automaticallly when there are JVM targets
    New: Adds generated code directories into sourceSets automatically
    
    Includes support for Java, Kotlin (MPP), and Android.
    Big thanks to SqlDelight.
    
    * Remove redundant Android deps and update AGP to 4.0.1
    
    * Use check
    
    * Fixing comments

--- wire-library/wire-gradle-plugin/src/test/kotlin/com/squareup/wire/gradle/WirePluginTest.kt
+++ wire-library/wire-gradle-plugin/src/test/kotlin/com/squareup/wire/gradle/WirePluginTest.kt
@@ -14,6 +14,7 @@ import kotlin.text.RegexOption.MULTILINE
 
 class WirePluginTest {
   private lateinit var gradleRunner: GradleRunner
+
   @Before
   fun setUp() {
     gradleRunner = GradleRunner.create()
@@ -34,9 +35,7 @@ class WirePluginTest {
     val result = gradleRunner.runFixture(fixtureRoot) { buildAndFail() }
 
     assertThat(result.task(":generateProtos")).isNull()
-    assertThat(result.output).contains(
-        """Either the Java or Kotlin plugin must be applied before the Wire Gradle plugin."""
-    )
+    assertThat(result.output).contains("Missing either the Java, Kotlin, or Android plugin")
   }
 
   @Test
@@ -83,7 +82,7 @@ class WirePluginTest {
 
     val result = gradleRunner.runFixture(fixtureRoot) { buildAndFail() }
 
-    val task = result.task(":generateProtos")
+    val task = result.task(":generateMainProtos")
     assertThat(task).isNotNull
     assertThat(result.output).contains("no sources")
   }
@@ -319,7 +318,8 @@ class WirePluginTest {
 
     assertThat(result.task(":generateProtos")).isNull()
     assertThat(result.output)
-        .contains("To generate Kotlin protos, please apply a Kotlin plugin.")
+        .contains(
+            "Wire Gradle plugin applied in project ':' but no supported Kotlin plugin was found")
   }
 
   @Test
@@ -439,7 +439,7 @@ class WirePluginTest {
       withArguments("run", "--stacktrace").build()
     }
 
-    assertThat(result.task(":generateProtos")).isNotNull
+    assertThat(result.task(":generateMainProtos")).isNotNull
     assertThat(result.output)
         .contains("Writing com.squareup.dinosaurs.Dinosaur")
         .contains("Writing com.squareup.geology.Period")
@@ -461,7 +461,7 @@ class WirePluginTest {
       withArguments("run", "--stacktrace").build()
     }
 
-    assertThat(result.task(":generateProtos")).isNotNull
+    assertThat(result.task(":generateMainProtos")).isNotNull
     assertThat(result.output)
         .contains("Writing com.squareup.dinosaurs.Dinosaur")
         .contains("Writing com.squareup.geology.Period")
@@ -483,7 +483,7 @@ class WirePluginTest {
       withArguments("run", "--stacktrace").build()
     }
 
-    assertThat(result.task(":generateProtos")).isNotNull
+    assertThat(result.task(":generateMainProtos")).isNotNull
     assertThat(result.output)
         .contains("Writing com.squareup.dinosaurs.Dinosaur")
         .contains("Writing com.squareup.geology.Period")
@@ -505,7 +505,7 @@ class WirePluginTest {
       withArguments("run", "--stacktrace").build()
     }
 
-    assertThat(result.task(":generateProtos")).isNotNull
+    assertThat(result.task(":generateMainProtos")).isNotNull
     assertThat(result.output)
         .contains("Writing com.squareup.dinosaurs.Dinosaur")
         .contains("Writing com.squareup.geology.Period")
@@ -655,7 +655,7 @@ class WirePluginTest {
 
     val result = gradleRunner.runFixture(fixtureRoot) { build() }
 
-    assertThat(result.task(":generateProtos")).isNotNull
+    assertThat(result.task(":generateMainProtos")).isNotNull
     assertThat(result.task(":helloWorld")).isNotNull
     assertThat(result.output)
         .contains("Writing com.squareup.dinosaurs.Dig")
@@ -722,7 +722,8 @@ class WirePluginTest {
       ).build()
     }
 
-    assertThat(result.task(":generateProtos")).isNotNull
+    println(result.tasks.joinToString { it.toString() })
+    assertThat(result.task(":generateJvmMainProtos")).isNotNull
     assertThat(result.output)
         .contains("Writing com.squareup.dinosaurs.Dinosaur")
         .contains("Writing com.squareup.geology.Period")
@@ -799,17 +800,15 @@ class WirePluginTest {
     val fixtureRoot = File("src/test/projects/consecutive-runs")
 
     val firstRun = gradleRunner.runFixture(fixtureRoot) { build() }
-
-    assertThat(firstRun.task(":generateProtos")).isNotNull
+    assertThat(firstRun.task(":generateMainProtos")).isNotNull
     assertThat(firstRun.output)
         .contains("Writing com.squareup.geology.Period")
         .contains("src/test/projects/consecutive-runs/custom")
 
     val secondRun = gradleRunner.runFixture(fixtureRoot) { build() }
-
-    assertThat(secondRun.task(":generateProtos")).isNotNull
+    assertThat(secondRun.task(":generateMainProtos")).isNotNull
     assertThat(secondRun.output)
-        .contains("Task :generateProtos UP-TO-DATE")
+        .contains("Task :generateMainProtos UP-TO-DATE")
   }
 
   @Test
