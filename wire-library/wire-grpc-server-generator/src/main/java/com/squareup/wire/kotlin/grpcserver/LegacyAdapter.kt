e0145e0370d70dc62270250ef44e4c3848689595
Ricardo Mariano <rmariano@squareup.com>
2021-01-19 16:37:35 +1100

    Add copyright notices, add margins to LegacyAdapter.kt

--- wire-library/wire-grpc-server-generator/src/main/java/com/squareup/wire/kotlin/grpcserver/LegacyAdapter.kt
+++ wire-library/wire-grpc-server-generator/src/main/java/com/squareup/wire/kotlin/grpcserver/LegacyAdapter.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2021 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.squareup.wire.kotlin.grpcserver
 
 import com.squareup.kotlinpoet.ClassName
@@ -92,39 +107,39 @@ object LegacyAdapter {
       val codeBlock = when {
         rpc.requestStreaming && rpc.responseStreaming -> CodeBlock.of(
           """
-          val requestStream = %T()
-          streamExecutor.submit {
-            ${rpc.name}().${rpc.name}(requestStream, %T(response))
-          }
-          return requestStream
-        """.trimIndent(),
+          |val requestStream = %T()
+          |streamExecutor.submit {
+          |  ${rpc.name}().${rpc.name}(requestStream, %T(response))
+          |}
+          |return requestStream
+          |""".trimMargin(),
           MessageSourceAdapter::class.asTypeName()
             .parameterizedBy(ClassName.bestGuess(rpc.requestType.toString())),
           MessageSinkAdapter::class.asTypeName()
         )
         rpc.requestStreaming -> CodeBlock.of(
           """
-          val requestStream = %T()
-          streamExecutor.submit {
-            response.onNext(${rpc.name}().${rpc.name}(requestStream))
-            response.onCompleted()
-          }
-          return requestStream
-          """.trimIndent(),
+          |val requestStream = %T()
+          |streamExecutor.submit {
+          |  response.onNext(${rpc.name}().${rpc.name}(requestStream))
+          |  response.onCompleted()
+          |}
+          |return requestStream
+          |""".trimMargin(),
           MessageSourceAdapter::class.asTypeName()
             .parameterizedBy(ClassName.bestGuess(rpc.requestType.toString()))
         )
         rpc.responseStreaming -> CodeBlock.of(
           """
-          ${rpc.name}().${rpc.name}(request, %T(response))
-          """.trimIndent(),
+          |${rpc.name}().${rpc.name}(request, %T(response))
+          |""".trimMargin(),
           MessageSinkAdapter::class.asTypeName()
         )
         else -> CodeBlock.of(
           """
-          response.onNext(${rpc.name}().${rpc.name}(request))
-          response.onCompleted()
-          """.trimIndent()
+          |response.onNext(${rpc.name}().${rpc.name}(request))
+          |response.onCompleted()
+          |""".trimMargin()
         )
       }
       builder.addFunction(
