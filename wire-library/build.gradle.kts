c5f11268e326b2136735854b6b1631ecd10e9ae9
Benoît Quenaudon <bquenaudon@squareup.com>
2021-12-15 00:38:31 +0100

    Remove bintray and jcenter (#2176)

--- wire-library/build.gradle.kts
+++ wire-library/build.gradle.kts
@@ -38,9 +38,6 @@ allprojects {
   repositories {
     mavenCentral()
     google()
-    jcenter()
-    maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
-    maven(url = "https://kotlin.bintray.com/kotlinx/")
   }
 }
 
