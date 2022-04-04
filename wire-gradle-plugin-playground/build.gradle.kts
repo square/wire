f92182bef59e4ba4cd0a6c9dda505c26438da1b4
Benoit Quenaudon <benoit@quenaudon.com>
2022-04-04 13:51:21 -0400

    Adds Wire Gradle plugin playground

---
+++ wire-gradle-plugin-playground/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+  id("java-library")
+  kotlin("jvm")
+  id("com.squareup.wire")
+}
+
+wire {
+  sourcePath {
+    srcDir("src/main/proto")
+  }
+
+  kotlin {
+  }
+}
+
+dependencies {
+  implementation(deps.wire.grpcClient)
+  implementation(deps.okio.core)
+  implementation(deps.wire.compiler)
+  implementation(deps.wire.gsonSupport)
+  implementation(deps.wire.moshiAdapter)
+  implementation(deps.assertj)
+  implementation(deps.junit)
+  implementation(deps.protobuf.javaUtil)
+  implementation(deps.wire.testUtils)
+}
