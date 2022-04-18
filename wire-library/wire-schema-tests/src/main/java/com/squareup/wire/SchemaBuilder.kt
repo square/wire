95e9d8eec0790b0227eb82efae8710db04eca9f0
Benoit Quenaudon <benoit@quenaudon.com>
2022-04-18 12:43:46 -0400

    Removed concept of protoPath in SchemaBuilder
    Schema is agnostic to sourcePath/protoPath

--- wire-library/wire-schema-tests/src/main/java/com/squareup/wire/SchemaBuilder.kt
+++ wire-library/wire-schema-tests/src/main/java/com/squareup/wire/SchemaBuilder.kt
@@ -9,46 +9,28 @@ import okio.Path
 import okio.Path.Companion.toPath
 import okio.fakefilesystem.FakeFileSystem
 
-/**
- * Builds a schema out of written `.proto` and `.wire` files.
- */
-class SchemaBuilder @JvmOverloads constructor(
-  private val sourcePath: Path = "/source".toPath(),
-  private val protoPath: Path = "/proto".toPath(),
-) {
+/** Builds a schema out of written `.proto` files. */
+class SchemaBuilder {
+  private val sourcePath: Path = "/source".toPath()
   private val fileSystem: FileSystem = FakeFileSystem()
 
   init {
     fileSystem.createDirectories(sourcePath)
-    fileSystem.createDirectories(protoPath)
   }
 
   /**
-   * Add a file to the `sourcePath`.
+   * Add a file to be loaded into the schema.
    * @param name The qualified name of the file. This can contain slashes.
    * @param protoFile The content of the file.
    */
   fun add(name: String, protoFile: String): SchemaBuilder {
-    return internalToRoot(sourcePath, name, protoFile)
-  }
-
-  /**
-   * Add a file to the `protoPath`.
-   * @param name The qualified name of the file. This can contain slashes.
-   * @param protoFile The content of the file.
-   */
-  fun addToProtoPath(name: String, protoFile: String): SchemaBuilder {
-    return internalToRoot(protoPath, name, protoFile)
-  }
-
-  private fun internalToRoot(root: Path, name: String, protoFile: String): SchemaBuilder {
     require(name.endsWith(".proto")) {
       "unexpected file extension for $name. Proto files should use the '.proto' extension"
     }
 
     val relativePath = name.toPath()
     try {
-      val resolvedPath = root / relativePath
+      val resolvedPath = sourcePath / relativePath
       val parent = resolvedPath.parent
       if (parent != null) {
         fileSystem.createDirectories(parent)
@@ -59,7 +41,6 @@ class SchemaBuilder @JvmOverloads constructor(
     } catch (e: IOException) {
       throw AssertionError(e)
     }
-
     return this
   }
 
@@ -67,16 +48,13 @@ class SchemaBuilder @JvmOverloads constructor(
     val schemaLoader = SchemaLoader(fileSystem)
     schemaLoader.initRoots(
       sourcePath = listOf(Location.get(sourcePath.toString())),
-      protoPath = listOf(Location.get(protoPath.toString())),
+      protoPath = listOf(),
     )
     return schemaLoader.loadSchema()
   }
 }
 
-inline fun buildSchema(
-  sourcePath: Path = "/source".toPath(),
-  protoPath: Path = "/proto".toPath(),
-  builderAction: SchemaBuilder.() -> Unit
-): Schema {
-  return SchemaBuilder(sourcePath, protoPath).apply(builderAction).build()
+/** Builds a schema out of written `.proto` files. */
+inline fun buildSchema(builderAction: SchemaBuilder.() -> Unit): Schema {
+  return SchemaBuilder().apply(builderAction).build()
 }
--- wire-library/wire-schema-tests/src/test/java/com/squareup/wire/SchemaBuilderTest.kt
+++ wire-library/wire-schema-tests/src/test/java/com/squareup/wire/SchemaBuilderTest.kt
@@ -3,7 +3,6 @@ package com.squareup.wire
 import com.squareup.wire.schema.Location
 import com.squareup.wire.schema.SchemaException
 import kotlin.test.assertFailsWith
-import okio.Path.Companion.toPath
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
@@ -11,23 +10,6 @@ class SchemaBuilderTest {
   @Test fun emptySchema() {
     val exception = assertFailsWith<SchemaException> {
       buildSchema {}
-    }
-    assertThat(exception.message).isEqualTo("no sources")
-  }
-
-  @Test fun protoPathOnly() {
-    val exception = assertFailsWith<SchemaException> {
-      buildSchema {
-        addToProtoPath(
-          "example2.proto",
-          """
-          |syntax = "proto2";
-          |
-          |message D {
-          |}
-          |""".trimMargin()
-        )
-      }
     }
     assertThat(exception.message).isEqualTo("no sources")
   }
@@ -62,41 +44,6 @@ class SchemaBuilderTest {
     assertThat(schema.protoFiles.map { it.location }).containsExactlyInAnyOrder(
       Location.get("/source", "example1.proto"),
       Location.get("/source", "example2.proto"),
-      Location.get("google/protobuf/descriptor.proto"),
-    )
-  }
-
-  @Test fun withProtoPath() {
-    val schema =
-      buildSchema(sourcePath = "/custom-source".toPath(), protoPath = "/custom-proto".toPath()) {
-        add(
-          "example1.proto",
-          """
-          |syntax = "proto2";
-          |
-          |import "example2.proto";
-          |
-          |message A {
-          |  optional B b = 1;
-          |}
-          |message B {
-          |  optional C c = 1;
-          |}
-          |""".trimMargin()
-        )
-        addToProtoPath(
-          "example2.proto",
-          """
-          |syntax = "proto2";
-          |
-          |message C {
-          |}
-          |""".trimMargin()
-        )
-      }
-    assertThat(schema.protoFiles.map { it.location }).containsExactlyInAnyOrder(
-      Location.get("/custom-source", "example1.proto"),
-      Location.get("/custom-proto", "example2.proto"),
       Location.get("google/protobuf/descriptor.proto"),
     )
   }
