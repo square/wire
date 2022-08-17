c9b852ce9d9d7c34a648c103bfe020f979686ba7
Alan Chiu <awlchiu@gmail.com>
2022-08-16 17:27:37 -0700

    integrate and call square code

--- wire-library/wire-protoc/src/main/java/com/squareup/wire/protocwire/WireGenerator.kt
+++ wire-library/wire-protoc/src/main/java/com/squareup/wire/protocwire/WireGenerator.kt
@@ -1,12 +1,16 @@
 package com.squareup.wire.protocwire
 
-import com.google.protobuf.Descriptors
+import com.google.protobuf.Descriptors.Descriptor
+import com.google.protobuf.Descriptors.FileDescriptor
+import com.squareup.wire.Syntax
+import com.squareup.wire.schema.*
+import com.squareup.wire.schema.internal.parser.MessageElement
+import com.squareup.wire.schema.internal.parser.ProtoFileElement
 
 class WireGenerator : CodeGenerator {
-    override fun generate(fileToGenerate: Descriptors.FileDescriptor, parameter: String, context: CodeGenerator.Context) {
-      println("++++++++++++++++++++++++++++")
-      context.addFile("hello.txt", "hello world")
-      println("++++++++++++++++++++++++++++")
+    override fun generate(fileToGenerate: FileDescriptor, parameter: String, context: CodeGenerator.Context) {
+      val protoFileElement = parseFileDescriptor(fileToGenerate)
+      context.addFile(fileToGenerate.name +".txt", protoFileElement.toSchema())
     }
 
     companion object {
@@ -15,4 +19,45 @@ class WireGenerator : CodeGenerator {
             Plugin.run(WireGenerator())
         }
     }
+}
+
+fun parseFileDescriptor(fileDescriptor: FileDescriptor): ProtoFileElement {
+  val location = Location.get(fileDescriptor.name)
+  val imports = mutableListOf<String>()
+  val publicImports = mutableListOf<String>()
+  val types = mutableListOf<MessageElement>()
+  val nestedTypes = mutableListOf<MessageElement>()
+
+  for (dependency in fileDescriptor.dependencies) {
+    imports.add(dependency.fullName)
+  }
+
+  for (publicDependency in fileDescriptor.publicDependencies) {
+    publicImports.add(publicDependency.fullName)
+  }
+
+  for (messageType in fileDescriptor.messageTypes) {
+    for (nestedType in messageType.nestedTypes) {
+      nestedTypes.add(parseMessage(nestedType))
+    }
+    types.add(parseMessage(messageType))
+  }
+  return ProtoFileElement(
+    location = location,
+    imports = imports,
+    publicImports = publicImports,
+    packageName = fileDescriptor.`package`,
+    types = types, // TODO: this is just to see if things break
+    services = emptyList(),
+    options = emptyList(),
+    syntax = Syntax.PROTO_3,
+  )
+}
+
+fun parseMessage(message: Descriptor): MessageElement {
+  return MessageElement(
+    location = Location.get(message.name),
+    name = message.name,
+    documentation = ""
+  )
 }
