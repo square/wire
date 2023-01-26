6c0e3ebe8ffbc6f3f59f358f52c10125a508352d
Benoit Quenaudon <benoit@quenaudon.com>
2023-01-26 21:36:40 +0100

    Logging for error

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ include(":wire-test-utils")
 include(":wire-tests")
 
 if (startParameter.projectProperties.get("swift") != "false") {
+  println("CHIRHO @${startParameter.projectProperties.get("swift")}@")
   include(":wire-runtime-swift")
   include(":wire-tests-swift")
   include(":wire-tests-proto3-swift")
