989b8233c25d54273f36e6995970eab2a20a2ea4
Alan Chiu <awlchiu@gmail.com>
2023-02-22 14:24:40 -0800

    more

--- wire-protoc-integration/src/main/java/com/squareup/wire/protocwire/Plugin.kt
+++ wire-protoc-integration/src/main/java/com/squareup/wire/protocwire/Plugin.kt
@@ -110,7 +110,7 @@ object Plugin {
         try {
             // go ahead and write response preamble
             PluginProtos.CodeGeneratorResponse
-                .newBuilder() // add more here as more features are introduced and then supported
+                .newBuilder()g
                 .setSupportedFeatures(
                     toFeatureBitmask(PluginProtos.CodeGeneratorResponse.Feature.FEATURE_PROTO3_OPTIONAL)
                 )
