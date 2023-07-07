15ff520db715fbdf7d869843f0eb92898644c67c
Benoit Quenaudon <benoit@quenaudon.com>
2023-07-07 12:09:41 +0200

    WireSchemaTests as MPP module

--- wire-schema-tests/build.gradle.kts
+++ wire-schema-tests/build.gradle.kts
@@ -2,10 +2,8 @@ import com.vanniktech.maven.publish.JavadocJar.Dokka
 import com.vanniktech.maven.publish.KotlinJvm
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
 
-// TODO(Benoit) this module can be multiplatform.
-
 plugins {
-  kotlin("jvm")
+  kotlin("multiplatform")
   id("org.jetbrains.dokka")
   id("com.vanniktech.maven.publish.base").apply(false)
 }
@@ -15,20 +13,112 @@ if (project.rootProject.name == "wire") {
   apply(plugin = "binary-compatibility-validator")
 }
 
-dependencies {
-  api(libs.junit)
-  api(projects.wireCompiler)
-  api(projects.wireSchema)
-  implementation(projects.wireKotlinGenerator)
-  implementation(projects.wireJavaGenerator)
-  implementation(projects.wireSwiftGenerator)
-  implementation(libs.okio.core)
-  implementation(libs.okio.fakefilesystem)
-  testImplementation(libs.assertj)
-  testImplementation(libs.kotlin.test.junit)
-  testImplementation(projects.wireTestUtils)
-}
+kotlin {
+  jvm {
+    withJava()
+  }
+  if (System.getProperty("kjs", "true").toBoolean()) {
+    js(IR) {
+      configure(listOf(compilations.getByName("main"), compilations.getByName("test"))) {
+        tasks.getByName(compileKotlinTaskName) {
+          kotlinOptions {
+            moduleKind = "umd"
+            sourceMap = true
+            metaInfo = true
+          }
+        }
+      }
+      nodejs()
+      browser()
+    }
+  }
+  if (System.getProperty("knative", "true").toBoolean()) {
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    // Required to generate tests tasks: https://youtrack.jetbrains.com/issue/KT-26547
+    linuxX64()
+    macosX64()
+    macosArm64()
+    mingwX64()
+    tvosX64()
+    tvosArm64()
+    tvosSimulatorArm64()
+  }
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        api(libs.okio.core)
+        api(projects.wireSchema)
+        implementation(libs.okio.fakefilesystem)
+      }
+    }
+    val commonTest by getting {
+      dependencies {
+        implementation(libs.kotlin.test.common)
+        implementation(libs.kotlin.test.annotations)
+      }
+    }
+    val jvmTest by getting {
+      dependencies {
+        implementation(libs.assertj)
+        implementation(libs.kotlin.test.junit)
+        implementation(projects.wireTestUtils)
+      }
+    }
+    if (System.getProperty("kjs", "true").toBoolean()) {
+      val jsTest by getting {
+        dependencies {
+          implementation(libs.kotlin.test.js)
+        }
+      }
+    }
+    if (System.getProperty("knative", "true").toBoolean()) {
+      val nativeMain by creating {
+        dependsOn(commonMain)
+      }
+      val nativeTest by creating {
+        dependsOn(commonTest)
+      }
+      val darwinMain by creating {
+        dependsOn(commonMain)
+      }
 
+      val iosX64Main by getting
+      val iosArm64Main by getting
+      val iosSimulatorArm64Main by getting
+      val linuxX64Main by getting
+      val macosX64Main by getting
+      val macosArm64Main by getting
+      val mingwX64Main by getting
+      val tvosX64Main by getting
+      val tvosArm64Main by getting
+      val tvosSimulatorArm64Main by getting
+      val iosX64Test by getting
+      val iosArm64Test by getting
+      val iosSimulatorArm64Test by getting
+      val linuxX64Test by getting
+      val macosX64Test by getting
+      val macosArm64Test by getting
+      val mingwX64Test by getting
+      val tvosX64Test by getting
+      val tvosArm64Test by getting
+      val tvosSimulatorArm64Test by getting
+
+      for (it in listOf(iosX64Main, iosArm64Main, iosSimulatorArm64Main, linuxX64Main, macosX64Main, macosArm64Main, mingwX64Main, tvosX64Main, tvosArm64Main, tvosSimulatorArm64Main)) {
+        it.dependsOn(nativeMain)
+      }
+
+      for (it in listOf(iosX64Test, iosArm64Test, iosSimulatorArm64Test, linuxX64Test, macosX64Test, macosArm64Test, mingwX64Test, tvosX64Test, tvosArm64Test, tvosSimulatorArm64Test)) {
+        it.dependsOn(nativeTest)
+      }
+
+      for (it in listOf(iosX64Main, iosArm64Main, macosX64Main, macosArm64Main, tvosX64Main, tvosArm64Main)) {
+        it.dependsOn(darwinMain)
+      }
+    }
+  }
+}
 
 if (project.rootProject.name == "wire") {
   configure<MavenPublishBaseExtension> {
--- wire-schema/build.gradle.kts
+++ wire-schema/build.gradle.kts
@@ -32,12 +32,27 @@ kotlin {
       browser()
     }
   }
+  if (System.getProperty("knative", "true").toBoolean()) {
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    // Required to generate tests tasks: https://youtrack.jetbrains.com/issue/KT-26547
+    linuxX64()
+    macosX64()
+    macosArm64()
+    mingwX64()
+    tvosX64()
+    tvosArm64()
+    tvosSimulatorArm64()
+  }
 
   sourceSets {
     val commonMain by getting {
       dependencies {
         api(projects.wireRuntime)
       }
+    }
+    val commonTest by getting {
     }
     val jvmMain by getting {
       dependencies {
@@ -64,6 +79,50 @@ kotlin {
         dependencies {
           implementation(libs.kotlin.test.js)
         }
+      }
+    }
+    if (System.getProperty("knative", "true").toBoolean()) {
+      val nativeMain by creating {
+        dependsOn(commonMain)
+      }
+      val nativeTest by creating {
+        dependsOn(commonTest)
+      }
+      val darwinMain by creating {
+        dependsOn(commonMain)
+      }
+
+      val iosX64Main by getting
+      val iosArm64Main by getting
+      val iosSimulatorArm64Main by getting
+      val linuxX64Main by getting
+      val macosX64Main by getting
+      val macosArm64Main by getting
+      val mingwX64Main by getting
+      val tvosX64Main by getting
+      val tvosArm64Main by getting
+      val tvosSimulatorArm64Main by getting
+      val iosX64Test by getting
+      val iosArm64Test by getting
+      val iosSimulatorArm64Test by getting
+      val linuxX64Test by getting
+      val macosX64Test by getting
+      val macosArm64Test by getting
+      val mingwX64Test by getting
+      val tvosX64Test by getting
+      val tvosArm64Test by getting
+      val tvosSimulatorArm64Test by getting
+
+      for (it in listOf(iosX64Main, iosArm64Main, iosSimulatorArm64Main, linuxX64Main, macosX64Main, macosArm64Main, mingwX64Main, tvosX64Main, tvosArm64Main, tvosSimulatorArm64Main)) {
+        it.dependsOn(nativeMain)
+      }
+
+      for (it in listOf(iosX64Test, iosArm64Test, iosSimulatorArm64Test, linuxX64Test, macosX64Test, macosArm64Test, mingwX64Test, tvosX64Test, tvosArm64Test, tvosSimulatorArm64Test)) {
+        it.dependsOn(nativeTest)
+      }
+
+      for (it in listOf(iosX64Main, iosArm64Main, macosX64Main, macosArm64Main, tvosX64Main, tvosArm64Main)) {
+        it.dependsOn(darwinMain)
       }
     }
   }
