a9e7316cc5d4a074404040e4eaa6597f944d7a9b
Benoît Quenaudon <benoit@quenaudon.com>
2024-11-26 15:12:43 +0000

    Promote google/protobuf/api.proto as a runtime proto
    this includes its dependencies. This is useful for people who want to define a path on their RPCs.

--- wire-schema/src/commonMain/kotlin/com/squareup/wire/schema/CoreLoader.kt
+++ wire-schema/src/commonMain/kotlin/com/squareup/wire/schema/CoreLoader.kt
@@ -33,23 +33,29 @@ fun isWireRuntimeProto(location: Location): Boolean {
 /** Returns true if [path] is bundled in the wire runtime. */
 fun isWireRuntimeProto(path: String): Boolean {
   return path == ANY_PROTO ||
+    path == API_PROTO ||
     path == DESCRIPTOR_PROTO ||
     path == DURATION_PROTO ||
     path == EMPTY_PROTO ||
+    path == SOURCE_CONTEXT_PROTO ||
     path == STRUCT_PROTO ||
     path == TIMESTAMP_PROTO ||
-    path == WRAPPERS_PROTO ||
-    path == WIRE_EXTENSIONS_PROTO
+    path == TYPE_PROTO ||
+    path == WIRE_EXTENSIONS_PROTO ||
+    path == WRAPPERS_PROTO
 }
 
 internal const val DESCRIPTOR_PROTO = "google/protobuf/descriptor.proto"
 internal const val WIRE_EXTENSIONS_PROTO = "wire/extensions.proto"
 
 private const val ANY_PROTO = "google/protobuf/any.proto"
+private const val API_PROTO = "google/protobuf/api.proto"
 private const val DURATION_PROTO = "google/protobuf/duration.proto"
 private const val EMPTY_PROTO = "google/protobuf/empty.proto"
+private const val SOURCE_CONTEXT_PROTO = "google/protobuf/source_context.proto"
 private const val STRUCT_PROTO = "google/protobuf/struct.proto"
 private const val TIMESTAMP_PROTO = "google/protobuf/timestamp.proto"
+private const val TYPE_PROTO = "google/protobuf/type.proto"
 private const val WRAPPERS_PROTO = "google/protobuf/wrappers.proto"
 
 /** A special base directory used for Wire's built-in .proto files. */
