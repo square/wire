410bdee9761da8901b294375af96ae37e962139f
DimaVilda <dimavilda@gmail.com>
2024-12-15 18:17:47 +0100

    NOTASK - apply spotless on a new file

--- wire-schema/src/commonTest/kotlin/com/squareup/wire/schema/LocationTest.kt
+++ wire-schema/src/commonTest/kotlin/com/squareup/wire/schema/LocationTest.kt
@@ -1,8 +1,23 @@
+/*
+ * Copyright (C) 2024 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.squareup.wire.schema
 
-import kotlin.test.Test
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import kotlin.test.Test
 
 class LocationTest {
 
@@ -15,7 +30,7 @@ class LocationTest {
   @Test fun getWithWindowsStylePaths() {
     val location = Location.get(
       "C:\\Users\\protoDir",
-      "languageDir\\language.proto"
+      "languageDir\\language.proto",
     )
     assertThat(location.base).isEqualTo("C:/Users/protoDir")
     assertThat(location.path).isEqualTo("languageDir/language.proto")
