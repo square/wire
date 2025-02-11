8bd253032095364f323090d83c602b7b78542bc7
Jeff Clyne <jclyne@squareup.com>
2025-02-11 10:40:53 -0600

    Cleanup comments

--- wire-grpc-client/src/commonMain/kotlin/com/squareup/wire/GrpcStreamingCallExtensions.kt
+++ wire-grpc-client/src/commonMain/kotlin/com/squareup/wire/GrpcStreamingCallExtensions.kt
@@ -20,14 +20,14 @@ import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.SendChannel
 
 /**
- * Executes a Client Streaming RPC call This encapsulates the Streaming request, single response use case for wire's
- * [GrpcStreamingCall] In this use case, all outbound request messages are sent before the inbound response message.
+ * Executes a Client Streaming RPC call
+ * In this use case, all outbound request messages are sent before the inbound response message.
  *
  * @param scope The [CoroutineScope] to use for the call. This will be used to create coroutines that will write the
  *   requests to the client message sync. It will also be the context for calling the [block] function
  * @param block A function that will be called with a [SendChannel] that the caller can use to send requests to the
  *   server. The channel will automatically be closed when the block completes.
- * @return The optional single response from the server
+ * @return The single response from the server
  */
 suspend inline fun <S : Any, R : Any> GrpcClientStreamingCall<S, R>.clientStream(
   scope: CoroutineScope,
@@ -43,13 +43,12 @@ suspend inline fun <S : Any, R : Any> GrpcClientStreamingCall<S, R>.clientStream
 }
 
 /**
- * Executes a blocking Client Streaming RPC call This encapsulates the Streaming request, single response use case for
- * wire's [GrpcStreamingCall] In this use case, all outbound request messages are sent before the inbound response
- * message.
+ * Executes a blocking Client Streaming RPC call
+ * In this use case, all outbound request messages are sent before the inbound response message.
  *
  * @param block A function that will be called with a [MessageSink] that the caller can use to send requests to the
- *   server. The channel will automatically be closed when the block completes.
- * @return The optional single response from the server
+ *   server. The sink will automatically be closed when the block completes.
+ * @return The single response from the server
  */
 inline fun <S : Any, R : Any> GrpcClientStreamingCall<S, R>.clientStreamBlocking(
   crossinline block: MessageSink<S>.() -> Unit,
@@ -64,10 +63,9 @@ inline fun <S : Any, R : Any> GrpcClientStreamingCall<S, R>.clientStreamBlocking
 }
 
 /**
- * Executes a Bidirectional Streaming RPC call This encapsulates the Streaming request, streaming response use case for
- * wire's [GrpcStreamingCall] In this use case, you are free to interleave request and response messages. The caller can
- * optionally send and receive messages in the supplied block or after the block completes in the returned
- * [ReceiveChannel].
+ * Executes a Bidirectional Streaming RPC call
+ * In this use case, you are free to interleave request and response messages. The caller can optionally send
+ * and receive messages in the supplied block or after the block completes in the returned [ReceiveChannel].
  *
  * @param scope The [CoroutineScope] to use for the call. This will be used to create coroutines that will write the
  *   requests to the client message sync. It will also be the context for calling the [block] function.
@@ -91,10 +89,9 @@ suspend inline fun <S : Any, R : Any> GrpcStreamingCall<S, R>.bidirectionalStrea
 }
 
 /**
- * Executes a blocking Bidirectional Streaming RPC call This encapsulates the Streaming request, streaming response use
- * case for wire's [GrpcStreamingCall] In this use case, you are free to interleave request and response messages. The
- * caller can optionally send and receive messages in the supplied block or after the block completes in the returned
- * [MessageSource].
+ * Executes a blocking Bidirectional Streaming RPC call
+ * In this use case, you are free to interleave request and response messages. The caller can optionally send and
+ * receive messages in the supplied block or after the block completes in the returned [MessageSource].
  *
  * @param block A function that will be called with a [MessageSink] and [MessageSource] that the caller can use to send
  *   requests to the server and consume responses from the server. The send channel will automatically be closed when
