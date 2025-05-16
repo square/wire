4e62bbf6772d0fbf1c572ee05828c19bf25c400d
Kartikaya Gupta <kartikaya@squareup.com>
2025-05-16 12:53:17 -0400

    Update InputLocation to be more cache-friendly
    Instead of storing individual resolved paths, we should use gradle's
    @Classpath annotation to indicate that we've got a set of input paths
    that are a classpath. Importantly, this will normalize files across
    varying gradle user home dirs, so jars that are resolved into the gradle
    user home dir will not have their full absolute path baked into the
    task input.

--- wire-gradle-plugin/src/main/kotlin/com/squareup/wire/gradle/WireTask.kt
+++ wire-gradle-plugin/src/main/kotlin/com/squareup/wire/gradle/WireTask.kt
@@ -34,6 +34,7 @@ import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.OutputDirectories
 import org.gradle.api.tasks.OutputDirectory
@@ -75,10 +76,10 @@ abstract class WireTask @Inject constructor(
   val pluginVersion: Property<String> = objects.property(String::class.java)
     .convention(VERSION)
 
-  @get:Input
+  @get:Nested
   internal abstract val sourceInput: ListProperty<InputLocation>
 
-  @get:Input
+  @get:Nested
   internal abstract val protoInput: ListProperty<InputLocation>
 
   @get:Input
@@ -156,11 +157,10 @@ abstract class WireTask @Inject constructor(
         }
     }
 
-    val projectDirAsFile = projectDir.asFile
     val allTargets = targets.get()
     val wireRun = WireRun(
-      sourcePath = sourceInput.get().flatMap { it.toLocations(fileOperations, projectDirAsFile) },
-      protoPath = protoInput.get().flatMap { it.toLocations(fileOperations, projectDirAsFile) },
+      sourcePath = sourceInput.get().flatMap { it.toLocations(fileOperations) },
+      protoPath = protoInput.get().flatMap { it.toLocations(fileOperations) },
       treeShakingRoots = roots.get().ifEmpty { includes },
       treeShakingRubbish = prunes.get().ifEmpty { excludes },
       moves = moves.get().map { it.toTypeMoverMove() },
