911ad381943c10d40304e930b0e470f56c54d40e
Benoît Quenaudon <benoit@quenaudon.com>
2026-04-16 12:11:02 +0100

    Use Kotlin 2.3's generatedKotlin API

---
+++ wire-gradle-plugin/src/test/projects/kotlin-project-kotlin-protos-kgp23/src/main/java/com/squareup/dinosaurs/Sample.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2026 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.squareup.dinosaurs
+
+import com.squareup.geology.Period
+import java.io.IOException
+import okio.ByteString.Companion.toByteString
+
+/* Note that we do not execute this class in tests. We're only testing that it compiles. */
+fun main(args: Array<String>) {
+  val stegosaurus = Dinosaur(
+    name = "Stegosaurus",
+    period = Period.JURASSIC,
+    length_meters = 9.0,
+    mass_kilograms = 5_000.0,
+    picture_urls = listOf("http://goo.gl/LD5KY5", "http://goo.gl/VYRM67"),
+  )
+  val stegosaurusEncoded = Dinosaur.ADAPTER.encode(stegosaurus)
+  println(stegosaurusEncoded.toByteString().base64())
+}
